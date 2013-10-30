931aadf4771f8c2362a9f1f629dc7668f622b5eb
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-10-30 01:08:02 -0400

    Fix Kangabean patching and enable loki ramdisk patching

--- patchinfo/jflte/ROMs/TouchWiz/kangabean.py
+++ patchinfo/jflte/ROMs/TouchWiz/kangabean.py
@@ -4,11 +4,10 @@ import re
 file_info = FileInfo()
 
 filename_regex           = r"KB-.*\.zip$"
-#file_info.ramdisk        = 'jflte/GoogleEdition/GoogleEdition.def'
-file_info.patch          = 'jflte/ROMs/TouchWiz/ge-jamal2367.dualboot.patch'
+file_info.ramdisk        = 'jflte/GoogleEdition/GoogleEdition.def'
+file_info.patch          = 'jflte/ROMs/TouchWiz/kangabean.dualboot.patch'
+file_info.bootimg        = "boot.lok"
 file_info.loki           = True
-#file_info.bootimg        = "boot.lok"
-file_info.has_boot_image = False
 
 def matches(filename):
   if re.search(filename_regex, filename):
