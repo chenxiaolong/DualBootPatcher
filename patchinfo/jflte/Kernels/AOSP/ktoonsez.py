c44033b510ab2beda9967f62190d91f8dbc26a59
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-10-30 00:28:02 -0400

    Add support for Loki'd ktoonsez kernels

--- patchinfo/jflte/Kernels/AOSP/ktoonsez.py
+++ patchinfo/jflte/Kernels/AOSP/ktoonsez.py
@@ -9,6 +9,10 @@ file_info.patch          = 'jflte/Kernels/AOSP/ktoonsez.dualboot.patch'
 
 def matches(filename):
   if re.search(filename_regex, filename):
+    if 'VZW' in filename or 'ATT' in filename:
+      file_info.patch    = 'jflte/Kernels/AOSP/ktoonsez-loki.dualboot.patch'
+      file_info.bootimg  = 'boot.lok'
+      file_info.loki     = True
     return True
   else:
     return False
