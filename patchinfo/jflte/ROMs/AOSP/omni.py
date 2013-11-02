dd73bd4ffd27644f867b34273c6a2e54e522cfc0
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-11-02 17:43:33 -0400

    Add support for Omni Kit Kat

--- patchinfo/jflte/ROMs/AOSP/omni.py
+++ patchinfo/jflte/ROMs/AOSP/omni.py
@@ -5,10 +5,13 @@ file_info = FileInfo()
 
 filename_regex           = r"^omni-[0-9\.]+-[0-9]+.*\.zip$"
 file_info.ramdisk        = 'jflte/AOSP/AOSP.def'
-file_info.patch          = 'jflte/ROMs/AOSP/omni.dualboot.patch'
 
 def matches(filename):
   if re.search(filename_regex, filename):
+    if '4.4' in filename:
+      file_info.patch    = 'jflte/ROMs/AOSP/omni-kitkat.dualboot.patch'
+    else:
+      file_info.patch    = 'jflte/ROMs/AOSP/omni.dualboot.patch'
     return True
   else:
     return False
