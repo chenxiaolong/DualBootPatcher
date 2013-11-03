c8e070f888053037ae392c3978704a07db6b1e5f
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-11-03 00:53:01 -0400

    Add support for loki'd ChronicKernel

--- patchinfo/jflte/Kernels/AOSP/chronickernel.py
+++ patchinfo/jflte/Kernels/AOSP/chronickernel.py
@@ -9,6 +9,10 @@ file_info.patch          = 'jflte/Kernels/AOSP/chronickernel.dualboot.patch'
 
 def matches(filename):
   if re.search(filename_regex, filename):
+    if 'VZW' in filename or 'ATT' in filename:
+      file_info.loki     = True
+      file_info.bootimg  = 'boot.lok'
+      file_info.patch    = 'jflte/Kernels/AOSP/chronickernel-loki.dualboot.patch'
     return True
   else:
     return False
