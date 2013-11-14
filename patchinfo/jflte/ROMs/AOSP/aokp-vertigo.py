735e7bb06a34228b42883a7a1e6ec00388f89105
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-11-13 21:08:40 -0500

    Patch some AOKP updater-scripts dynamically

--- patchinfo/jflte/ROMs/AOSP/aokp-vertigo.py
+++ patchinfo/jflte/ROMs/AOSP/aokp-vertigo.py
@@ -1,11 +1,11 @@
 from fileinfo import FileInfo
+import common as c
 import re
 
 file_info = FileInfo()
 
 filename_regex           = r"^aokp_ICJ.*\.zip$"
 file_info.ramdisk        = 'jflte/AOSP/AOSP.def'
-file_info.patch          = 'jflte/ROMs/AOSP/aokp.dualboot.patch'
 
 def matches(filename):
   if re.search(filename_regex, filename):
@@ -18,3 +18,13 @@ def print_message():
 
 def get_file_info():
   return file_info
+
+def extract_files():
+  return [ 'META-INF/com/google/android/updater-script' ]
+
+def patch_files(directory):
+  lines = c.get_lines_from_file(directory, 'META-INF/com/google/android/updater-script')
+
+  c.attempt_auto_patch(lines, bootimg = file_info.bootimg)
+
+  c.write_lines_to_file(directory, 'META-INF/com/google/android/updater-script', lines)
