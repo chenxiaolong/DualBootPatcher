e6379cb83576b0228ff22f73659552f3b99146a0
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-11-13 22:37:40 -0500

    Dynamically patch JellyBeer's updater-script

--- patchinfo/jflte/ROMs/AOSP/jellybeer.py
+++ patchinfo/jflte/ROMs/AOSP/jellybeer.py
@@ -1,11 +1,11 @@
 from fileinfo import FileInfo
+import common as c
 import re
 
 file_info = FileInfo()
 
 filename_regex           = r"^JellyBeer-.*\.zip$"
 file_info.ramdisk        = 'jflte/AOSP/AOSP.def'
-file_info.patch          = 'jflte/ROMs/AOSP/jellybeer.dualboot.patch'
 file_info.need_new_init  = True
 
 def matches(filename):
@@ -19,3 +19,13 @@ def print_message():
 
 def get_file_info():
   return file_info
+
+def extract_files():
+  return [ 'META-INF/com/google/android/updater-script' ]
+
+def patch_files(directory):
+  lines = c.get_lines_from_file(directory, 'META-INF/com/google/android/updater-script')
+
+  c.attempt_auto_patch(lines, bootimg = file_info.bootimg)
+
+  c.write_lines_to_file(directory, 'META-INF/com/google/android/updater-script', lines)
