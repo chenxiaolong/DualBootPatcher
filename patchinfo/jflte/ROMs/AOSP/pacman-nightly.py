fdca694dd567e0e7a0c229ae867e1e47ea66f27b
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-11-17 17:32:14 -0500

    Pacman nightlies: don't remove device asserts

--- patchinfo/jflte/ROMs/AOSP/pacman-nightly.py
+++ patchinfo/jflte/ROMs/AOSP/pacman-nightly.py
@@ -8,7 +8,6 @@ filename_regex           = r"^pac_[a-z0-9]+-nightly-[0-9]+.zip$"
 file_info.ramdisk        = 'jflte/AOSP/AOSP.def'
 file_info.patch          = c.auto_patch
 file_info.extract        = c.files_to_auto_patch
-file_info.device_check   = False
 
 def matches(filename):
   if re.search(filename_regex, filename):
