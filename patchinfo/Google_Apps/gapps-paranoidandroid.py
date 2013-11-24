f837850fee2d1dce97774243cb5403ffeb456a35
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-11-24 16:15:15 -0500

    Auto-patch Paranoid Android Google Apps package

--- patchinfo/Google_Apps/gapps-paranoidandroid.py
+++ patchinfo/Google_Apps/gapps-paranoidandroid.py
@@ -1,10 +1,12 @@
 from fileinfo import FileInfo
+import common as c
 import re
 
 file_info = FileInfo()
 
 filename_regex           = r"^pa_gapps-full-4\.[34]-[0-9]+-signed\.zip$"
-file_info.patch          = 'Google_Apps/gapps-paranoidandroid.patch'
+file_info.patch          = c.auto_patch
+file_info.extract        = c.files_to_auto_patch
 file_info.has_boot_image = False
 
 def matches(filename):
