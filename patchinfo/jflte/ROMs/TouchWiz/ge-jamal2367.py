42c7be26c8d497900c0b3fe4b99b253526efcc19
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-11-23 22:38:50 -0500

    Auto-patch jamal2367's Google Edition ROM

--- patchinfo/jflte/ROMs/TouchWiz/ge-jamal2367.py
+++ patchinfo/jflte/ROMs/TouchWiz/ge-jamal2367.py
@@ -1,11 +1,14 @@
 from fileinfo import FileInfo
+import common as c
 import re
 
 file_info = FileInfo()
 
 filename_regex           = r"^I9505_-_Official_Google_Edition_.*Jamal2367.*\.zip$"
 file_info.ramdisk        = 'jflte/GoogleEdition/GoogleEdition.def'
-file_info.patch          = 'jflte/ROMs/TouchWiz/ge-jamal2367.dualboot.patch'
+#file_info.patch          = 'jflte/ROMs/TouchWiz/ge-jamal2367.dualboot.patch'
+file_info.patch          = c.auto_patch
+file_info.extract        = c.files_to_auto_patch
 
 def matches(filename):
   if re.search(filename_regex, filename):
