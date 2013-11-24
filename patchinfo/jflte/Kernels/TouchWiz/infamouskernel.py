7fba041ad78676332c88f0c5b46ff7f52cac5b4f
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-11-24 00:43:50 -0500

    Auto-patch infamous kernel

--- patchinfo/jflte/Kernels/TouchWiz/infamouskernel.py
+++ patchinfo/jflte/Kernels/TouchWiz/infamouskernel.py
@@ -1,11 +1,13 @@
 from fileinfo import FileInfo
+import common as c
 import re
 
 file_info = FileInfo()
 
 filename_regex           = r"^Infamous_S4_Kernel.v.*\.zip$"
 file_info.ramdisk        = 'jflte/TouchWiz/TouchWiz.def'
-file_info.patch          = 'jflte/Kernels/TouchWiz/infamouskernel.dualboot.patch'
+file_info.patch          = c.auto_patch
+file_info.extract        = c.files_to_auto_patch
 
 def matches(filename):
   if re.search(filename_regex, filename):
