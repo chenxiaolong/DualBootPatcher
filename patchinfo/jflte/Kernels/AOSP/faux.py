1223e446553f0bba9c4ec1cdb51c4709a6f8b4b0
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-11-23 23:43:31 -0500

    Auto-patch faux kernel

--- patchinfo/jflte/Kernels/AOSP/faux.py
+++ patchinfo/jflte/Kernels/AOSP/faux.py
@@ -1,14 +1,19 @@
 from fileinfo import FileInfo
+import common as c
 import re
 
 file_info = FileInfo()
 
 filename_regex           = r"^jflte[a-z]+-aosp-faux123-.*\.zip$"
 file_info.ramdisk        = 'jflte/AOSP/AOSP.def'
-file_info.patch          = 'jflte/Kernels/AOSP/faux.dualboot.patch'
+file_info.patch          = c.auto_patch
+file_info.extract        = c.files_to_auto_patch
 
 def matches(filename):
   if re.search(filename_regex, filename):
+    if 'vzw' in filename or 'att' in filename:
+      file_info.bootimg  = 'boot.lok'
+      file_info.loki     = True
     return True
   else:
     return False
