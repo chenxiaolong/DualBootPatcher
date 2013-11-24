f5b6ff65fa79947f55a6aa4413b0c9f0aace134b
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-11-23 23:33:45 -0500

    Auto-patch ktoonsez AOSP kernels

--- patchinfo/jflte/Kernels/AOSP/ktoonsez.py
+++ patchinfo/jflte/Kernels/AOSP/ktoonsez.py
@@ -1,16 +1,17 @@
 from fileinfo import FileInfo
+import common as c
 import re
 
 file_info = FileInfo()
 
-filename_regex           = r"^KT-SGS4-JB4.3-AOSP-.*\.zip$"
+filename_regex           = r"^KT-SGS4-[^-]+-AOSP-.*\.zip$"
 file_info.ramdisk        = 'jflte/AOSP/AOSP.def'
-file_info.patch          = 'jflte/Kernels/AOSP/ktoonsez.dualboot.patch'
+file_info.patch          = c.auto_patch
+file_info.extract        = c.files_to_auto_patch
 
 def matches(filename):
   if re.search(filename_regex, filename):
     if 'VZW' in filename or 'ATT' in filename:
-      file_info.patch    = 'jflte/Kernels/AOSP/ktoonsez-loki.dualboot.patch'
       file_info.bootimg  = 'boot.lok'
       file_info.loki     = True
     return True
