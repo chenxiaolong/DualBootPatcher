ab184241f0943e114c4d9e5a60536d65319c5135
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-11-24 15:55:53 -0500

    Auto-patch Triforce ROM

--- patchinfo/jflte/ROMs/TouchWiz/triforce-update.py
+++ patchinfo/jflte/ROMs/TouchWiz/triforce-update.py
@@ -1,12 +1,13 @@
 from fileinfo import FileInfo
+import common as c
 import re
 
 file_info = FileInfo()
 
 filename_regex           = r"^TriForceROM[0-9\.]+Update\.zip$"
 file_info.ramdisk        = 'jflte/TouchWiz/TouchWiz.def'
-file_info.patch          = 'jflte/ROMs/TouchWiz/triforce-update.dualboot.patch'
-file_info.bootimg        = "aroma/kernels/stock/boot.img"
+file_info.patch          = [ c.auto_patch ]
+file_info.extract        = c.files_to_auto_patch
 
 def matches(filename):
   if re.search(filename_regex, filename):
@@ -19,3 +20,21 @@ def print_message():
 
 def get_file_info():
   return file_info
+
+def fix_aroma(directory, bootimg = None, device_check = True,
+              partition_config = None):
+  lines = c.get_lines_from_file(directory, 'META-INF/com/google/android/updater-script')
+
+  i = 0
+  while i < len(lines):
+    if re.search('getprop.*/system/build.prop', lines[i]):
+      i += c.insert_mount_system(i, lines)
+      i += c.insert_mount_cache(i, lines)
+      i += c.insert_mount_data(i, lines)
+      lines[i] = re.sub('/system', partition_config.target_system, lines[i])
+
+    i += 1
+
+  c.write_lines_to_file(directory, 'META-INF/com/google/android/updater-script', lines)
+
+file_info.patch.append(fix_aroma)
--- patchinfo/jflte/ROMs/TouchWiz/triforce.py
+++ patchinfo/jflte/ROMs/TouchWiz/triforce.py
@@ -1,11 +1,14 @@
 from fileinfo import FileInfo
+import common as c
 import re
 
 file_info = FileInfo()
 
 filename_regex           = r"^TriForceROM[0-9\.]+\.zip$"
 file_info.ramdisk        = 'jflte/TouchWiz/TouchWiz.def'
-file_info.patch          = 'jflte/ROMs/TouchWiz/triforce.dualboot.patch'
+file_info.patch          = [ c.auto_patch ]
+file_info.extract        = [ c.files_to_auto_patch,
+                             'META-INF/com/google/android/aroma-config' ]
 file_info.bootimg        = "aroma/kernels/stock/boot.img"
 
 def matches(filename):
@@ -19,3 +22,27 @@ def print_message():
 
 def get_file_info():
   return file_info
+
+def fix_aroma(directory, bootimg = None, device_check = True,
+              partition_config = None):
+  lines = c.get_lines_from_file(directory, 'META-INF/com/google/android/aroma-config')
+
+  i = 0
+  while i < len(lines):
+    if re.search('/system/build.prop', lines[i]):
+      # Remove 'raw-' since aroma mounts the partitions directly
+      target_dir = re.sub("raw-", "", partition_config.target_system)
+      lines[i] = re.sub('/system', target_dir, lines[i])
+
+    elif re.search(r"/sbin/mount.*/system", lines[i]):
+      i += c.insert_line(i + 1, re.sub('/system', '/cache', lines[i]), lines)
+      i += c.insert_line(i + 1, re.sub('/system', '/data', lines[i]), lines)
+
+    elif '~welcome.title' in lines[i]:
+      i += c.insert_line(i + 1, '"***IMPORTANT***: You MUST choose the stock kernel for dual booting to work. If you want to use a custom kernel, you can patch and flash it afterwards.\\n\\n" +', lines)
+
+    i += 1
+
+  c.write_lines_to_file(directory, 'META-INF/com/google/android/aroma-config', lines)
+
+file_info.patch.append(fix_aroma)
