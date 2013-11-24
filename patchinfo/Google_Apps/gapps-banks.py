8a996aac270d1b51728b529eb92beffffa1fef59
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-11-24 18:59:12 -0500

    Auto-patch BaNkS' Google Apps package

--- patchinfo/Google_Apps/gapps-banks.py
+++ patchinfo/Google_Apps/gapps-banks.py
@@ -1,10 +1,12 @@
 from fileinfo import FileInfo
+import common as c
 import re
 
 file_info = FileInfo()
 
 filename_regex           = r"^[0-9]+-[0-9]+_GApps_Core_[0-9\.]+_signed\.zip$"
-file_info.patch          = 'Google_Apps/gapps-banks.dualboot.patch'
+file_info.patch          = c.auto_patch
+file_info.extract        = c.files_to_auto_patch
 file_info.has_boot_image = False
 
 def matches(filename):
