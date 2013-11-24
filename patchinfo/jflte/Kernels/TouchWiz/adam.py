db3657c8a6d8a80ab1b5b56d5b02dd9762f363b7
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-11-24 00:34:31 -0500

    Auto-patch Adam kernel

--- patchinfo/jflte/Kernels/TouchWiz/adam.py
+++ patchinfo/jflte/Kernels/TouchWiz/adam.py
@@ -1,11 +1,13 @@
 from fileinfo import FileInfo
+import common as c
 import re
 
 file_info = FileInfo()
 
 filename_regex           = r"^.*_AdamKernel.V[0-9\.]+\.CWM\.zip$"
 file_info.ramdisk        = 'jflte/TouchWiz/TouchWiz.def'
-file_info.patch          = 'jflte/Kernels/TouchWiz/adam.dualboot.patch'
+file_info.patch          = c.auto_patch
+file_info.extract        = c.files_to_auto_patch
 file_info.bootimg        = "wanam/boot.img"
 
 def matches(filename):
