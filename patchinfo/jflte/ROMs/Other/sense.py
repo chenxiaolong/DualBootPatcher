2c4f8a75a21ca288bf94fbccb213dd9bc5fe273c
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-11-25 18:30:37 -0500

    Auto-patch HTC Sense 5 port

--- patchinfo/jflte/ROMs/Other/sense.py
+++ patchinfo/jflte/ROMs/Other/sense.py
@@ -1,13 +1,16 @@
 from fileinfo import FileInfo
+import common as c
 
 file_info = FileInfo()
 
 file_info.ramdisk        = 'jflte/AOSP/AOSP.def'
-file_info.patch          = 'jflte/ROMs/Other/sense-geiti94.dualboot.patch'
+file_info.patch          = c.auto_patch
+file_info.extract        = c.files_to_auto_patch
 
 def matches(filename):
   if filename.lower() == "sense5port.zip" or \
-     filename         == "4.3 official port.zip":
+     filename         == "4.3 official port.zip" or \
+     filename         == "ALPHA V1.5.zip":
     return True
   else:
     return False
