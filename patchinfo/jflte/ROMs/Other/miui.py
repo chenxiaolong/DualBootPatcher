2daf0b838f46a550a65f20b66ce3efaeb9078f58
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-11-25 04:48:56 -0500

    Auto-patch MIUI

--- patchinfo/jflte/ROMs/Other/miui.py
+++ patchinfo/jflte/ROMs/Other/miui.py
@@ -1,11 +1,13 @@
 from fileinfo import FileInfo
+import common as c
 import re
 
 file_info = FileInfo()
 
-filename_regex           = r"^miuiandroid_jflte.*\.zip$"
+filename_regex           = r"^miuiandroid_(multi_)?jflte.*\.zip$"
 file_info.ramdisk        = 'jflte/AOSP/AOSP.def'
-file_info.patch          = 'jflte/ROMs/Other/miui.dualboot.patch'
+file_info.patch          = c.auto_patch
+file_info.extract        = c.files_to_auto_patch
 file_info.need_new_init  = True
 
 def matches(filename):
