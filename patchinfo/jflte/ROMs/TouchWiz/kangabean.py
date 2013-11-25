17284703f90a7cc3a6c7bd1b7fc76cd75b269822
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-11-25 18:53:00 -0500

    Auto-patch Kangabean

--- patchinfo/jflte/ROMs/TouchWiz/kangabean.py
+++ patchinfo/jflte/ROMs/TouchWiz/kangabean.py
@@ -1,11 +1,13 @@
 from fileinfo import FileInfo
+import common as c
 import re
 
 file_info = FileInfo()
 
 filename_regex           = r"KB-.*\.zip$"
 file_info.ramdisk        = 'jflte/GoogleEdition/GoogleEdition.def'
-file_info.patch          = 'jflte/ROMs/TouchWiz/kangabean.dualboot.patch'
+file_info.patch          = c.auto_patch
+file_info.extract        = c.files_to_auto_patch
 file_info.bootimg        = "boot.lok"
 file_info.loki           = True
 
