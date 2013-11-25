7f3673a813fc64c492f0248a8fa05997b59c0813
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-11-24 19:08:39 -0500

    Auto-patch doomed151 Google Apps package

--- patchinfo/Google_Apps/gapps-doomed151.py
+++ patchinfo/Google_Apps/gapps-doomed151.py
@@ -1,10 +1,12 @@
 from fileinfo import FileInfo
+import common as c
 import re
 
 file_info = FileInfo()
 
 filename_regex           = r"^gapps-jb43-[0-9]+-dmd151\.zip$"
-file_info.patch          = 'Google_Apps/gapps-doomed151.dualboot.patch'
+file_info.patch          = c.auto_patch
+file_info.extract        = c.files_to_auto_patch
 file_info.has_boot_image = False
 
 def matches(filename):
