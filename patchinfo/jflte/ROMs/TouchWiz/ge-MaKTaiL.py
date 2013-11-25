72e6336b0ff4d8c8dac4a160ee20a89c75f4e069
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-11-25 18:41:55 -0500

    Auto-patch MaKTaiL's Google Edition ROM

--- patchinfo/jflte/ROMs/TouchWiz/ge-MaKTaiL.py
+++ patchinfo/jflte/ROMs/TouchWiz/ge-MaKTaiL.py
@@ -1,11 +1,13 @@
 from fileinfo import FileInfo
+import common as c
 import re
 
 file_info = FileInfo()
 
 filename_regex           = r"^i9505-ge-untouched-4.3-.*.zip$"
 file_info.ramdisk        = 'jflte/GoogleEdition/GoogleEdition.def'
-file_info.patch          = 'jflte/ROMs/TouchWiz/ge-MaKTaiL.dualboot.patch'
+file_info.patch          = c.auto_patch
+file_info.extract        = c.files_to_auto_patch
 
 def matches(filename):
   if re.search(filename_regex, filename):
