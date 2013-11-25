11601d1ea5ade575dec95e8fb1e5745891e66f2a
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-11-24 19:00:59 -0500

    Auto-patch MIUI Google Apps package

--- patchinfo/Google_Apps/gapps-miui.py
+++ patchinfo/Google_Apps/gapps-miui.py
@@ -1,10 +1,12 @@
 from fileinfo import FileInfo
+import common as c
 import re
 
 file_info = FileInfo()
 
 filename_regex           = r"^miuiandroid_gapps.*\.zip$"
-file_info.patch          = 'GoogleApps/gapps-miui.dualboot.patch'
+file_info.patch          = c.auto_patch
+file_info.extract        = c.files_to_auto_patch
 file_info.has_boot_image = False
 
 def matches(filename):
