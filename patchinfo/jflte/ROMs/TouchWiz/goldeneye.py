3241e6b3b5283404d2028666f30beb58bd1cb18f
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-11-25 18:15:19 -0500

    Fix auto-patching of GoldenEye

--- patchinfo/jflte/ROMs/TouchWiz/goldeneye.py
+++ patchinfo/jflte/ROMs/TouchWiz/goldeneye.py
@@ -6,13 +6,12 @@ file_info = FileInfo()
 
 filename_regex           = r"^GE.*\.zip$"
 file_info.ramdisk        = 'jflte/TouchWiz/TouchWiz.def'
-file_info.has_boot_image = True
+file_info.patch          = c.auto_patch
+file_info.extract        = c.files_to_auto_patch
 file_info.bootimg        = 'kernel/boot.img'
 
 def print_message():
   print("Detected Goldeneye")
-
-###
 
 def matches(filename):
   if re.search(filename_regex, filename):
@@ -25,13 +24,3 @@ def matches(filename):
 
 def get_file_info():
   return file_info
-
-def extract_files():
-  return [ 'META-INF/com/google/android/updater-script' ]
-
-def patch_files(directory):
-  lines = c.get_lines_from_file(directory, 'META-INF/com/google/android/updater-script')
-
-  c.attempt_auto_patch(lines, bootimg = file_info.bootimg)
-
-  c.write_lines_to_file(directory, 'META-INF/com/google/android/updater-script', lines)
