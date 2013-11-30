263822ae78079d4426031d77377c029f28f0c618
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-11-30 03:34:58 -0500

    Oops

--- scripts/patchfile.py
+++ scripts/patchfile.py
@@ -683,7 +683,7 @@ if __name__ == "__main__":
   fileinfo = get_file_info(filename)
 
   if ('all' not in fileinfo.configs and partition_config not in fileinfo.configs) \
-      or '!' + partition_config in fileinfo.configs:
+      or '!' + partition_config.id in fileinfo.configs:
     fileinfo = None
     print_i("The %s partition configuration is not supported for this file" % partition_config.id)
 
