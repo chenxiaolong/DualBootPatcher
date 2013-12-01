f128aaab618ccdcc42e12bd74321ae8bf4b957a4
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-12-01 17:54:57 -0500

    Fix mediaserver crash in jamal2367's Google Edition 4.4 ROM

--- patchinfo/jflte/ROMs/TouchWiz/ge-jamal2367.py
+++ patchinfo/jflte/ROMs/TouchWiz/ge-jamal2367.py
@@ -6,9 +6,8 @@ file_info = FileInfo()
 
 filename_regex           = r"^I9505_-_Official_Google_Edition_.*Jamal2367.*\.zip$"
 file_info.ramdisk        = 'jflte/GoogleEdition/GoogleEdition.def'
-#file_info.patch          = 'jflte/ROMs/TouchWiz/ge-jamal2367.dualboot.patch'
-file_info.patch          = c.auto_patch
-file_info.extract        = c.files_to_auto_patch
+file_info.patch          = [ c.auto_patch ]
+file_info.extract        = [ c.files_to_auto_patch, 'system/etc/init.qcom.audio.sh' ]
 
 def matches(filename):
   if re.search(filename_regex, filename):
@@ -21,3 +20,21 @@ def print_message():
 
 def get_file_info():
   return file_info
+
+# WTF? Script removes system/etc/snd_soc_msm/snd_soc_msm_2x_Fusion3_auxpcm
+# causing the ROM to fail to boot
+def unbootable(directory, bootimg = None, device_check = True,
+               partition_config = None):
+  lines = c.get_lines_from_file(directory, 'system/etc/init.qcom.audio.sh')
+
+  i = 0
+  while i < len(lines):
+    if 'snd_soc_msm_2x_Fusion3_auxpcm' in lines[i]:
+      del lines[i]
+
+    else:
+      i += 1
+
+  c.write_lines_to_file(directory, 'system/etc/init.qcom.audio.sh', lines)
+
+file_info.patch.append(unbootable)
