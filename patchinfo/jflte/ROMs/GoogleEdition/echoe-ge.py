9355f31453bae77145d3b6f1eec01f655faffc7d
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-12-07 16:27:10 -0500

    Add support for Echoe Google Edition ROM

---
+++ patchinfo/jflte/ROMs/GoogleEdition/echoe-ge.py
@@ -0,0 +1,40 @@
+from fileinfo import FileInfo
+import common as c
+import re
+
+file_info = FileInfo()
+
+filename_regex           = r"^Echoe_KitKat.*\.zip$"
+file_info.ramdisk        = 'jflte/GoogleEdition/GoogleEdition.def'
+file_info.patch          = [ c.auto_patch ]
+file_info.extract        = [ c.files_to_auto_patch, 'system/etc/init.qcom.audio.sh' ]
+
+def matches(filename):
+  if re.search(filename_regex, filename):
+    return True
+  else:
+    return False
+
+def print_message():
+  print("Detected Echoe Google Edition ROM zip")
+
+def get_file_info():
+  return file_info
+
+# WTF? Script removes system/etc/snd_soc_msm/snd_soc_msm_2x_Fusion3_auxpcm
+# causing the ROM to fail to boot
+def unbootable(directory, bootimg = None, device_check = True,
+               partition_config = None):
+  lines = c.get_lines_from_file(directory, 'system/etc/init.qcom.audio.sh')
+
+  i = 0
+  while i < len(lines):
+    if 'snd_soc_msm_2x_Fusion3_auxpcm' in lines[i]:
+      del lines[i]
+
+    else:
+      i += 1
+
+  c.write_lines_to_file(directory, 'system/etc/init.qcom.audio.sh', lines)
+
+file_info.patch.append(unbootable)
