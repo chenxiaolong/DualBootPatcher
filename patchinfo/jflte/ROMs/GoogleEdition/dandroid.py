07733d781f99fe8e6cfeb70eceaa6c0b1f7696f7
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-12-31 01:33:53 -0500

    jflte: Dandroid is named differently now

--- patchinfo/jflte/ROMs/GoogleEdition/dandroid.py
+++ patchinfo/jflte/ROMs/GoogleEdition/dandroid.py
@@ -5,7 +5,7 @@ import re
 
 file_info = FileInfo()
 
-filename_regex           = r"^Dandroid.*\.zip$"
+filename_regex           = r"^(danvdh-GE|Dandroid).*\.zip$"
 file_info.ramdisk        = 'jflte/GoogleEdition/GoogleEdition.def'
 file_info.patch          = [ autopatcher.auto_patch, GoogleEditionPatcher.qcom_audio_fix ]
 file_info.extract        = [ autopatcher.files_to_auto_patch, GoogleEditionPatcher.files_for_qcom_audio_fix ]
