b9abf0973c1065b8c4b1a80049b768af57dfc79d
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2014-01-04 13:03:46 -0500

    One more regex for TouchWiz Echoe

--- patchinfo/jflte/ROMs/TouchWiz/echoe.py
+++ patchinfo/jflte/ROMs/TouchWiz/echoe.py
@@ -8,7 +8,8 @@ file_info = FileInfo()
 # Just so I don't need to redownload, illusion filenames:
 #   Echoe_Illusion_RomTHEMED_v7_09Nov_Vzw_v2.zip
 #   AT&T_Echoe_Illusion_Rom_v7_09Nov.zip
-filename_regex           = r"^Echoe[ _]?(Rom|SLIM).*\.zip$"
+filename_regex           = [ r"^Echoe[ _]?(Rom|SLIM).*\.zip$",
+                             r"^S4_Echoe.*\.zip" ]
 file_info.ramdisk        = 'jflte/TouchWiz/TouchWiz.def'
 file_info.patch          = autopatcher.auto_patch
 file_info.extract        = autopatcher.files_to_auto_patch
@@ -18,10 +19,10 @@ def print_message():
   print("Detected Echoe TouchWiz ROM zip")
 
 def matches(filename):
-  if re.search(filename_regex, filename):
-    return True
-  else:
-    return False
+  for regex in filename_regex:
+    if re.search(regex, filename):
+      return True
+  return False
 
 def get_file_info():
   return file_info
