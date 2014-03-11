c785d0a64e79bae0eeb198d2de2019b739895905
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2014-03-11 16:49:01 -0400

    jflte: Add support for ktoonsez's TouchWiz 4.4 kernel

--- patchinfo/jflte/Kernels/TouchWiz/ktoonsez-tw.py
+++ patchinfo/jflte/Kernels/TouchWiz/ktoonsez-tw.py
@@ -4,16 +4,14 @@ import re
 
 file_info = FileInfo()
 
-filename_regex_42        = r"^KT-SGS4-JB4.2-TW-.*\.zip$"
-filename_regex_43        = r"^KT-SGS4-JB4.3-TW-.*\.zip$"
+filename_regex           = r"^KT-SGS4-(JB|KK)4.[2-4]-TW-.*\.zip$"
 file_info.name           = 'Ktoonsez TouchWiz kernel'
 file_info.ramdisk        = 'jflte/TouchWiz/TouchWiz.def'
 file_info.patch          = autopatcher.auto_patch
 file_info.extract        = autopatcher.files_to_auto_patch
 
 def matches(filename):
-  if re.search(filename_regex_42, filename) or \
-      re.search(filename_regex_43, filename):
+  if re.search(filename_regex, filename):
     if 'VZW' in filename or 'ATT' in filename:
       file_info.bootimg  = 'boot.lok'
       file_info.loki     = True
