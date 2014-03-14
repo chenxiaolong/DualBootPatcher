76b451c94055bf086788971bd017b89829f9839e
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2014-03-14 15:00:33 -0400

    jflte: Add support for PAC Black Power Edition NG

--- patchinfo/jflte/ROMs/AOSP/pacman-metaiiica.py
+++ patchinfo/jflte/ROMs/AOSP/pacman-metaiiica.py
@@ -4,7 +4,7 @@ import re
 
 file_info = FileInfo()
 
-filename_regex           = r"^pac_.*-Black-Power-Edition_[0-9]+\.zip$"
+filename_regex           = r"^pac_.*-Black-Power-Edition(_NG)?[-_][0-9]+\.zip$"
 file_info.name           = "Metaiiica's PAC-Man"
 file_info.ramdisk        = 'jflte/AOSP/AOSP.def'
 file_info.patch          = autopatcher.auto_patch
