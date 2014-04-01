0f6e24a16111a05d7293ff201ff49fa77976da31
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2014-04-01 14:19:06 -0400

    falcon: Add support for Slim

---
+++ patchinfo/falcon/ROMs/AOSP/slim.py
@@ -0,0 +1,20 @@
+from multiboot.fileinfo import FileInfo
+import multiboot.autopatcher as autopatcher
+import re
+
+file_info = FileInfo()
+
+filename_regex           = r"^Slim-.*.zip$"
+file_info.name           = 'SlimRoms'
+file_info.ramdisk        = 'falcon/AOSP/AOSP.def'
+file_info.patch          = autopatcher.auto_patch
+file_info.extract        = autopatcher.files_to_auto_patch
+
+def matches(filename):
+  if re.search(filename_regex, filename):
+    return True
+  else:
+    return False
+
+def get_file_info():
+  return file_info
