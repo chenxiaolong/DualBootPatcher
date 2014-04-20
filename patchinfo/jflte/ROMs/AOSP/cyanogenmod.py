0dd9d4c144a1f68a8b857fa7bce7c6d9faacbe11
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2014-04-20 14:22:28 -0400

    jflte: Add support for CyanogenMod RC releases

--- patchinfo/jflte/ROMs/AOSP/cyanogenmod.py
+++ patchinfo/jflte/ROMs/AOSP/cyanogenmod.py
@@ -4,7 +4,7 @@ import re
 
 file_info = FileInfo()
 
-filename_regex           = r"^cm-[0-9\.]+(-[0-9]+-NIGHTLY)?-[a-z0-9]+\.zip$"
+filename_regex           = r"^cm-[0-9\.]+(-[0-9]+-NIGHTLY|-RC[0-9]+)?-[a-z0-9]+\.zip$"
 file_info.name           = 'Official CyanogenMod'
 file_info.ramdisk        = 'jflte/AOSP/AOSP.def'
 file_info.patch          = autopatcher.auto_patch
