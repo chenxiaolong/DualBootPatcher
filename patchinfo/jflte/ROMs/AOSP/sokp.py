f8cb70b45f8e5eca5efcdb8147cb24ce63fcce72
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2014-05-11 20:23:42 -0400

    jflte: AOKP should be SOKP

--- patchinfo/jflte/ROMs/AOSP/sokp.py
+++ patchinfo/jflte/ROMs/AOSP/sokp.py
@@ -5,7 +5,7 @@ import re
 file_info = FileInfo()
 
 filename_regex           = r"^SOKP.+-jflte\.zip$"
-file_info.name           = 'AOKP'
+file_info.name           = 'SOKP'
 file_info.ramdisk        = 'jflte/AOSP/AOSP.def'
 file_info.patch          = autopatcher.auto_patch
 file_info.extract        = autopatcher.files_to_auto_patch
