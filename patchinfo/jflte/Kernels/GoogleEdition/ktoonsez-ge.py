69beb0a9beaa9b7148afeb3d92940638617fa433
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2014-05-22 17:19:24 -0400

    jflte: Ktoonsez's boot images are no longer pre-loki'd

--- patchinfo/jflte/Kernels/GoogleEdition/ktoonsez-ge.py
+++ patchinfo/jflte/Kernels/GoogleEdition/ktoonsez-ge.py
@@ -1,6 +1,5 @@
 from multiboot.fileinfo import FileInfo
 import multiboot.autopatcher as autopatcher
-import os
 
 file_info = FileInfo()
 
@@ -9,13 +8,3 @@ file_info.name           = 'Ktoonsez Google Edition kernel'
 file_info.ramdisk        = 'jflte/GoogleEdition/GoogleEdition.def'
 file_info.patch          = autopatcher.auto_patch
 file_info.extract        = autopatcher.files_to_auto_patch
-
-def get_file_info(filename):
-  if not filename:
-    return file_info
-
-  filename = os.path.split(filename)[1]
-  if 'VZW' in filename or 'ATT' in filename:
-    file_info.bootimg    = 'boot.lok'
-    file_info.loki       = True
-  return file_info
