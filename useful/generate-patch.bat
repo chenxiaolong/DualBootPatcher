64b04e7bd55d36a993420064ea5156e174090b05
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2014-05-22 20:19:07 -0400

    Follow GNU's recommendations and add the GPLv3+ header to all the source code

--- useful/generate-patch.bat
+++ useful/generate-patch.bat
@@ -1,3 +1,18 @@
+rem Copyright (C) 2014  Xiao-Long Chen <chenxiaolong@cxl.epac.to>
+rem
+rem This program is free software: you can redistribute it and/or modify
+rem it under the terms of the GNU General Public License as published by
+rem the Free Software Foundation, either version 3 of the License, or
+rem (at your option) any later version.
+rem
+rem This program is distributed in the hope that it will be useful,
+rem but WITHOUT ANY WARRANTY; without even the implied warranty of
+rem MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+rem GNU General Public License for more details.
+rem
+rem You should have received a copy of the GNU General Public License
+rem along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 @echo off
 "%~dp0\..\pythonportable\python.exe" -B "%~dp0\..\scripts\gendiff.py" %*
 echo.
