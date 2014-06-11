815586d4ece1665ce8506a256e043733db533803
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2014-06-10 21:47:04 -0400

    jflte: Echoe renamed their ROMs again

--- patchinfo/jflte/ROMs/GoogleEdition/echoe-ge.py
+++ patchinfo/jflte/ROMs/GoogleEdition/echoe-ge.py
@@ -1,11 +1,19 @@
 from multiboot.patchinfo import PatchInfo
 import multiboot.autopatcher as autopatcher
 from multiboot.autopatchers.jflte import GoogleEditionPatcher
+import os
+import re
 
 patchinfo = PatchInfo()
 
-patchinfo.matches        = r"^Echoe_KitKat.*\.zip$"
 patchinfo.name           = 'Echoe Google Edition'
 patchinfo.ramdisk        = 'jflte/GoogleEdition/GoogleEdition.def'
 patchinfo.patch          = [ autopatcher.auto_patch, GoogleEditionPatcher.qcom_audio_fix ]
 patchinfo.extract        = [ autopatcher.files_to_auto_patch, GoogleEditionPatcher.files_for_qcom_audio_fix ]
+
+def matches(filename):
+  filename = os.path.split(filename)[1]
+  return re.search(r'^Echoe_KitKat.+\.zip$', filename) is not None or \
+    re.search(r'^EchoeGE.+\.zip$', filename) is not None
+
+patchinfo.matches        = matches
