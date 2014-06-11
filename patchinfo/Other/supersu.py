e1e4032449ec39883012ae8b39509355d65b9a8d
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2014-06-10 21:41:27 -0400

    Add support for SuperSU release candidates

--- patchinfo/Other/supersu.py
+++ patchinfo/Other/supersu.py
@@ -2,7 +2,7 @@ from multiboot.patchinfo import PatchInfo
 
 patchinfo = PatchInfo()
 
-patchinfo.matches        = r"^UPDATE-SuperSU-v[0-9\.]+\.zip$"
+patchinfo.matches        = r"^UPDATE-SuperSU-v[0-9A-Za-z\.]+\.zip$"
 patchinfo.name           = "Chainfire's SuperSU"
 patchinfo.patch          = 'Other/supersu.dualboot.patch'
 patchinfo.has_boot_image = False
