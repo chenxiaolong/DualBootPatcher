52acac02f0ab25ab0e88e8533973e86c70c21a29
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2014-06-11 21:58:21 -0400

    jflte: Use CyanogenMod init binary (the original in V2.6.7la is broken)

--- patchinfo/jflte/Kernels/AOSP/alucard-aosp.py
+++ patchinfo/jflte/Kernels/AOSP/alucard-aosp.py
@@ -8,3 +8,4 @@ patchinfo.name           = 'Alucard AOSP kernel'
 patchinfo.ramdisk        = 'jflte/AOSP/AOSP.def'
 patchinfo.patch          = autopatcher.auto_patch
 patchinfo.extract        = autopatcher.files_to_auto_patch
+patchinfo.patched_init   = 'init-kk44'
