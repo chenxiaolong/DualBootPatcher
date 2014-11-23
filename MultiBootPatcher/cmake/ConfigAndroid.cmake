1f49864dd6153ad6596b129f3ce28b7be9c631d0
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2014-11-23 02:22:35 -0500

    Allow Android libs to be built with debugging symbols enabled

--- MultiBootPatcher/cmake/ConfigAndroid.cmake
+++ MultiBootPatcher/cmake/ConfigAndroid.cmake
@@ -8,3 +8,12 @@ find_program(NDK_BUILD ndk-build
 if(NOT NDK_BUILD)
     message(FATAL_ERROR "Could NOT find Android NDK (missing: ndk-build)")
 endif()
+
+# Pass NDK_DEBUG=1 to ndk-build commands
+option(ANDROID_DEBUG "Compile Android targets with debugging symbols" OFF)
+
+set(NDK_ARGS "")
+
+if(ANDROID_DEBUG)
+    set(NDK_ARGS "${NDK_ARGS} NDK_DEBUG=1")
+endif()
