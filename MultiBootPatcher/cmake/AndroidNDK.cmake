3a105ac4272b2bef80de67b67ca8ce2d78581ac3
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2014-12-04 02:20:18 -0500

    CMake: syncdaemon should always be built

---
+++ MultiBootPatcher/cmake/AndroidNDK.cmake
@@ -0,0 +1,19 @@
+# Need ndk-build for building the Android stuff
+find_program(NDK_BUILD ndk-build
+    HINTS
+    "$ENV{ANDROID_NDK_HOME}"
+    "$ENV{ANDROID_NDK}"
+)
+
+if(NOT NDK_BUILD)
+    message(FATAL_ERROR "Could NOT find Android NDK (missing: ndk-build)")
+endif()
+
+# Pass NDK_DEBUG=1 to ndk-build commands
+option(ANDROID_DEBUG "Compile Android targets with debugging symbols" OFF)
+
+set(NDK_ARGS "")
+
+if(ANDROID_DEBUG)
+    set(NDK_ARGS "${NDK_ARGS} NDK_DEBUG=1")
+endif()
--- MultiBootPatcher/cmake/ConfigAndroid.cmake
+++ MultiBootPatcher/cmake/ConfigAndroid.cmake
@@ -1,23 +1,3 @@
-# Need ndk-build for building the Android stuff
-find_program(NDK_BUILD ndk-build
-    HINTS
-    "$ENV{ANDROID_NDK_HOME}"
-    "$ENV{ANDROID_NDK}"
-)
-
-if(NOT NDK_BUILD)
-    message(FATAL_ERROR "Could NOT find Android NDK (missing: ndk-build)")
-endif()
-
-# Pass NDK_DEBUG=1 to ndk-build commands
-option(ANDROID_DEBUG "Compile Android targets with debugging symbols" OFF)
-
-set(NDK_ARGS "")
-
-if(ANDROID_DEBUG)
-    set(NDK_ARGS "${NDK_ARGS} NDK_DEBUG=1")
-endif()
-
 # Install paths
 set(DATA_INSTALL_DIR .)
 
