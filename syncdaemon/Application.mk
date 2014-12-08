86d7160132eadb5c40fdac50aa71f8d9fa80f077
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2014-12-08 01:01:15 -0500

    CMake fixes to integrate Android build process a little better

--- syncdaemon/Application.mk
+++ syncdaemon/Application.mk
@@ -1,10 +1,11 @@
 APP_BUILD_SCRIPT := Android.mk
 
-APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
+#APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
+APP_ABI := armeabi-v7a x86 x86_64
 
-APP_PLATFORM := android-17
+APP_PLATFORM := android-21
 
 # GNU libstdc++ is needed for C++11's <thread>
 APP_STL := gnustl_static
 APP_CPPFLAGS := -std=c++11
-NDK_TOOLCHAIN_VERSION := 4.8
+NDK_TOOLCHAIN_VERSION := 4.9
