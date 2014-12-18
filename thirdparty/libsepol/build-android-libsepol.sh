615d8fc10e2acb035dd141e7cbb5803c73d57e0b
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2014-12-17 19:09:15 -0500

    thirdparty: Switch to stable 2.3 version of libsepol

--- thirdparty/libsepol/build-android-libsepol.sh
+++ thirdparty/libsepol/build-android-libsepol.sh
@@ -15,18 +15,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# Chainfire's repo is the same as the upstream repo except that it has commit
-# da6e0d7ee142806e5f80ffdc3253535ea0f13159, which contains some build fixes
-# for Android.
-
-#url="https://github.com/SELinuxProject/selinux.git"
-#branch="master"
-#commit="b1bbd3030be095b5e5c49c6f899ed8071fb05f30"
-url="https://github.com/Chainfire/selinux.git"
-branch="android"
-commit="da6e0d7ee142806e5f80ffdc3253535ea0f13159"
-
-curdate="$(date +%Y%m%d)"
+url="https://github.com/SELinuxProject/selinux.git"
+ver="2.3"
 
 set -e
 
@@ -34,13 +24,14 @@ if [ ! -d selinux ]; then
     git clone "${url}" selinux
 else
     pushd selinux
-    git checkout "${branch}"
+    git checkout master
     git pull
     popd
 fi
 
 pushd selinux/libsepol
-git checkout "${commit}"
+git checkout "libsepol-${ver}"
+git am ../../0001-Build-for-Android-targets.patch
 git am ../../0001-Build-static-library.patch
 
 arches=(armeabi-v7a arm64-v8a x86 x86_64)
@@ -63,10 +54,10 @@ for arch in "${arches[@]}"; do
     cp obj/local/${arch}/libsepol.a "${outdir}"/lib_${arch}/
 done
 
-if [ ! -f ../../libsepol_${curdate}_android.tar.bz2 ]; then
+if [ ! -f ../../libsepol-${ver}_android.tar.bz2 ]; then
     curdir="$(pwd)"
     pushd "${outdir}"
-    tar jcvf "${curdir}"/../../libsepol_${curdate}_android.tar.bz2 \
+    tar jcvf "${curdir}"/../../libsepol-${ver}_android.tar.bz2 \
         lib_*/ include/
     popd
 fi
