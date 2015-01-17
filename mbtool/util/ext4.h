c6a4f3e72f2b53e162f568f3bf790a66f3825c3a
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2015-01-17 01:53:27 -0500

    mbtool: Add function for reading some information from the ext4 superblock

---
+++ mbtool/util/ext4.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2014  Xiao-Long Chen <chenxiaolong@cxl.epac.to>
+ *
+ * This file is part of MultiBootPatcher
+ *
+ * MultiBootPatcher is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MultiBootPatcher is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MultiBootPatcher.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <inttypes.h>
+
+struct ext4_info {
+    uint16_t magic;
+    uint32_t block_size;
+    uint32_t block_count;
+};
+
+int mb_ext4_get_info(const char *path, struct ext4_info *info);
