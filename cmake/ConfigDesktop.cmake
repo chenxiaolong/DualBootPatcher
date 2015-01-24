250fc1198e391129504b1857769135f3221211ed
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2015-01-23 19:36:44 -0500

    Use bundled external libraries by default
    This is needed so we can use features in unreleased versions of
    libarchive and also to make compiling easier on Windows.
    
    The system versions of the following libraries can be used by passing
    these options to CMake:
    
    libarchive: -DMBP_USE_SYSTEM_LIBRARY_LIBARCHIVE=ON
    liblzma:    -DMBP_USE_SYSTEM_LIBRARY_LIBLZMA=ON
    lz4:        -DMBP_USE_SYSTEM_LIBRARY_LZ4=ON
    zlib:       -DMBP_USE_SYSTEM_LIBRARY_ZLIB=ON
    
    Alternatively, to always use system libraries, just pass
    -DMBP_USE_SYSTEM_LIBRARIES=ON to CMake.

--- cmake/ConfigDesktop.cmake
+++ cmake/ConfigDesktop.cmake
@@ -1,3 +1,4 @@
+include(CMakeDependentOption)
 include(GNUInstallDirs)
 
 # Portable application
@@ -54,12 +55,191 @@ link_directories(${Boost_LIBRARY_DIRS})
 
 find_package(Qt5Core 5.3 REQUIRED)
 
-find_package(LibArchive REQUIRED)
-include_directories(${LibArchive_INCLUDE_DIRS})
-link_directories(${LibArchive_LIBRARY_DIRS})
-
 find_package(LibXml2 REQUIRED)
 include_directories(${LIBXML2_INCLUDE_DIR})
+
+
+# Same logic as CMakeLists.txt from the CMake source
+set(EXTERNAL_LIBRARIES LIBARCHIVE LIBLZMA LZ4 ZLIB)
+foreach(extlib ${EXTERNAL_LIBRARIES})
+    if(NOT DEFINED MBP_USE_SYSTEM_LIBRARY_${extlib}
+            AND DEFINED MBP_USE_SYSTEM_LIBRARIES)
+        set(MBP_USE_SYSTEM_LIBRARY_${extlib} "${MBP_USE_SYSTEM_LIBRARIES}")
+    endif()
+    if(DEFINED MBP_USE_SYSTEM_LIBRARY_${extlib})
+        if(MBP_USE_SYSTEM_LIBRARY_${extlib})
+            set(MBP_USE_SYSTEM_LIBRARY_${extlib} ON)
+        else()
+            set(MBP_USE_SYSTEM_LIBRARY_${extlib} OFF)
+        endif()
+        string(TOLOWER "${extlib}" extlib_lower)
+        set(MBP_USE_SYSTEM_${extlib} "${MBP_USE_SYSTEM_LIBRARY_${extlib}}"
+            CACHE BOOL "Use system-installed ${extlib_lower}" FORCE)
+    else()
+        set(MBP_USE_SYSTEM_LIBRARY_${extlib} OFF)
+    endif()
+endforeach()
+
+
+# Optionally use system utility libraries.
+option(
+    MBP_USE_SYSTEM_LIBARCHIVE
+    "Use system-installed libarchive"
+    "${MBP_USE_SYSTEM_LIBRARY_LIBARCHIVE}"
+)
+cmake_dependent_option(
+    MBP_USE_SYSTEM_ZLIB
+    "Use system-installed zlib"
+    "${MBP_USE_SYSTEM_LIBRARY_ZLIB}"
+    "NOT MBP_USE_SYSTEM_LIBARCHIVE"
+    ON
+)
+cmake_dependent_option(
+    MBP_USE_SYSTEM_BZIP2
+    "Use system-installed bzip2"
+    "${MBP_USE_SYSTEM_LIBRARY_BZIP2}"
+    "NOT MBP_USE_SYSTEM_LIBARCHIVE"
+    ON
+)
+cmake_dependent_option(
+    MBP_USE_SYSTEM_LIBLZMA
+    "Use system-installed liblzma"
+    "${MBP_USE_SYSTEM_LIBRARY_LIBLZMA}"
+    "NOT MBP_USE_SYSTEM_LIBARCHIVE"
+    ON
+)
+
+foreach(extlib ${EXTERNAL_LIBRARIES})
+    if(MBP_USE_SYSTEM_${extlib})
+        message(STATUS "Using system-installed ${extlib}")
+    endif()
+endforeach()
+
+
+# zlib
+if(MBP_USE_SYSTEM_ZLIB)
+    find_package(ZLIB)
+    if(NOT ZLIB_FOUND)
+        message(FATAL_ERROR "MBP_USE_SYSTEM_ZLIB is ON but zlib is not found!")
+    endif()
+    set(MBP_ZLIB_INCLUDES ${ZLIB_INCLUDE_DIR})
+    set(MBP_ZLIB_LIBRARIES ${ZLIB_LIBRARIES})
+else()
+    set(SKIP_INSTALL_ALL ON CACHE INTERNAL "Disable zlib install")
+    set(MBP_ZLIB_INCLUDES ${CMAKE_SOURCE_DIR}/external/zlib)
+    set(MBP_ZLIB_LIBRARIES zlibstatic)
+    add_subdirectory(external/zlib)
+    # Linking shared library to zlib's static library, need -fPIC
+    set_target_properties(
+        zlibstatic
+        PROPERTIES
+        POSITION_INDEPENDENT_CODE 1
+    )
+    # Don't build the shared library or other binaries
+    set_target_properties(
+        zlib example example64 minigzip minigzip64
+        PROPERTIES
+        EXCLUDE_FROM_ALL 1
+        EXCLUDE_FROM_DEFAULT_BUILD 1
+    )
+endif()
+
+
+# liblzma
+if(MBP_USE_SYSTEM_LIBLZMA)
+    find_package(LibLZMA)
+    if(NOT LIBLZMA_FOUND)
+        message(FATAL_ERROR "CMAKE_USE_SYSTEM_LIBLZMA is ON but LibLZMA is not found!")
+    endif()
+    set(MBP_LIBLZMA_INCLUDES ${LIBLZMA_INCLUDE_DIRS})
+    set(MBP_LIBLZMA_LIBRARIES ${LIBLZMA_LIBRARIES})
+else()
+    add_subdirectory(external/xz)
+    set(MBP_LIBLZMA_INCLUDES "${CMAKE_SOURCE_DIR}/external/xz/src/liblzma/api")
+    set(MBP_LIBLZMA_LIBRARIES lzma_static)
+    # Linking shared library to libarchive's static library, need -fPIC
+    set_target_properties(
+        lzma_static
+        PROPERTIES
+        POSITION_INDEPENDENT_CODE 1
+    )
+    # Don't build the shared library
+    set_target_properties(
+        lzma
+        PROPERTIES
+        EXCLUDE_FROM_ALL 1
+        EXCLUDE_FROM_DEFAULT_BUILD 1
+    )
+endif()
+
+
+# lz4
+if(MBP_USE_SYSTEM_LZ4)
+    find_path(MBP_LZ4_INCLUDES lz4.h)
+    find_library(MBP_LZ4_LIBRARIES NAMES lz4 liblz4)
+    include(FindPackageHandleStandardArgs)
+    find_package_handle_standard_args(LZ4 DEFAULT_MSG MBP_LZ4_LIBRARIES MBP_LZ4_INCLUDES)
+    if(NOT LZ4_FOUND)
+        message(FATAL_ERROR "CMAKE_USE_SYSTEM_LIBLZ4 is ON but LZ4 is not found!")
+    endif()
+else()
+    set(BUILD_TOOLS OFF CACHE INTERNAL "Build the command line tools")
+    set(BUILD_LIBS ON CACHE INTERNAL "Build the libraries in addition to the tools")
+    add_subdirectory(external/lz4/cmake_unofficial)
+    set(MBP_LZ4_INCLUDES "${CMAKE_SOURCE_DIR}/external/lz4/lib")
+    set(MBP_LZ4_LIBRARIES liblz4)
+endif()
+
+
+# libarchive
+if(MBP_USE_SYSTEM_LIBARCHIVE)
+    find_package(LibArchive)
+    if(NOT LibArchive_FOUND)
+        message(FATAL_ERROR "MBP_USE_SYSTEM_LIBARCHIVE is ON but LibArchive is not found!")
+    endif()
+    set(MBP_LIBARCHIVE_INCLUDES ${LibArchive_INCLUDE_DIRS})
+    set(MBP_LIBARCHIVE_LIBRARIES ${LibArchive_LIBRARIES})
+else()
+    set(ENABLE_NETTLE OFF CACHE INTERNAL "Enable use of Nettle")
+    set(ENABLE_OPENSSL OFF CACHE INTERNAL "Enable use of OpenSSL")
+    set(ENABLE_LZMA ON CACHE INTERNAL "Enable the use of the system found LZMA library if found")
+    set(ENABLE_ZLIB ON CACHE INTERNAL "Enable the use of the system found ZLIB library if found")
+    set(ENABLE_BZip2 OFF CACHE INTERNAL "Enable the use of the system found BZip2 library if found")
+    set(ENABLE_EXPAT OFF CACHE INTERNAL "Enable the use of the system found EXPAT library if found")
+    set(ENABLE_PCREPOSIX OFF CACHE INTERNAL "Enable the use of the system found PCREPOSIX library if found")
+    set(ENABLE_LibGCC OFF CACHE INTERNAL "Enable the use of the system found LibGCC library if found")
+    set(ENABLE_XATTR OFF CACHE INTERNAL "Enable extended attribute support")
+    set(ENABLE_ACL OFF CACHE INTERNAL "Enable ACL support")
+    set(ENABLE_ICONV OFF CACHE INTERNAL "Enable iconv support")
+    set(ENABLE_TAR OFF CACHE INTERNAL "Enable tar building")
+    set(ENABLE_CPIO OFF CACHE INTERNAL "Enable cpio building")
+    set(ENABLE_CAT OFF CACHE INTERNAL "Enable cat building")
+    set(ENABLE_TEST OFF CACHE INTERNAL "Enable unit and regression tests")
+    set(ENABLE_INSTALL OFF CACHE INTERNAL "Enable installing of libraries")
+    set(ZLIB_INCLUDE_DIR ${MBP_ZLIB_INCLUDES})
+    set(ZLIB_LIBRARY ${MBP_ZLIB_LIBRARIES})
+    set(LZMA_INCLUDE_DIR ${MBP_LIBLZMA_INCLUDES})
+    set(LZMA_LIBRARY ${MBP_LIBLZMA_LIBRARIES})
+    set(LZ4_INCLUDE_DIR ${MBP_LZ4_INCLUDES})
+    set(LZ4_LIBRARY ${MBP_LZ4_LIBRARIES})
+    set(MBP_LIBARCHIVE_INCLUDES ${CMAKE_SOURCE_DIR}/external/libarchive)
+    set(MBP_LIBARCHIVE_LIBRARIES archive_static)
+    add_subdirectory(external/libarchive)
+    # Linking shared library to libarchive's static library, need -fPIC
+    set_target_properties(
+        archive_static
+        PROPERTIES
+        POSITION_INDEPENDENT_CODE 1
+    )
+    # Don't build the shared library
+    set_target_properties(
+        archive
+        PROPERTIES
+        EXCLUDE_FROM_ALL 1
+        EXCLUDE_FROM_DEFAULT_BUILD 1
+    )
+endif()
+
 
 # Install paths
 
