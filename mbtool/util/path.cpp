0cb587e52377317107eff226953b98c13152a599
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2015-02-11 21:05:05 -0500

    mbtool: Add wrapper for getcwd()

---
+++ mbtool/util/path.cpp
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2014  Andrew Gunnerson <andrewgunnerson@gmail.com>
+ *
+ * This file is part of MultiBootPatcher
+ *
+ * MultiBootPatcher is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MultiBootPatcher is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MultiBootPatcher.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "util/path.h"
+
+#include <cerrno>
+
+#include "util/logging.h"
+
+namespace MB {
+
+std::string get_cwd()
+{
+    char *cwd = NULL;
+
+    if (!(cwd = getcwd(NULL, 0))) {
+        LOGE("Failed to get cwd: %s", strerror(errno));
+        return std::string();
+    }
+
+    std::string ret(cwd);
+    free(cwd);
+    return ret;
+}
+
+}
