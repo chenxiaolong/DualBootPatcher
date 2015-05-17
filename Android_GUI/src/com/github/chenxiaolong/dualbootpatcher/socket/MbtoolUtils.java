ca1ef74eefd319b12249f8cc637cbf60e6bd6653
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2015-05-17 17:29:20 -0400

    Android GUI: Bump minimum app sharing version to 8.0.0.r1261 for the hard link fixes

--- Android_GUI/src/com/github/chenxiaolong/dualbootpatcher/socket/MbtoolUtils.java
+++ Android_GUI/src/com/github/chenxiaolong/dualbootpatcher/socket/MbtoolUtils.java
@@ -32,7 +32,7 @@ public class MbtoolUtils {
         if (BuildConfig.BUILD_TYPE.equals("ci")) {
             // Snapshot builds
             sMinVersionMap.put(Feature.DAEMON, Version.from("8.0.0.r1243"));
-            sMinVersionMap.put(Feature.APP_SHARING, Version.from("8.0.0.r1255"));
+            sMinVersionMap.put(Feature.APP_SHARING, Version.from("8.0.0.r1261"));
             sMinVersionMap.put(Feature.IN_APP_INSTALLATION, Version.from("8.0.0.r1248"));
         } else {
             // Debug/release builds
