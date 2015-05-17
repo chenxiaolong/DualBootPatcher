3ce0a6d22c9bb733212be980daf61dc148e5fd65
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2015-05-17 18:01:02 -0400

    Android GUI: Bump minimum daemon version to 8.0.0.r1264 for the SELinux fixes

--- Android_GUI/src/com/github/chenxiaolong/dualbootpatcher/socket/MbtoolUtils.java
+++ Android_GUI/src/com/github/chenxiaolong/dualbootpatcher/socket/MbtoolUtils.java
@@ -31,7 +31,7 @@ public class MbtoolUtils {
     static {
         if (BuildConfig.BUILD_TYPE.equals("ci")) {
             // Snapshot builds
-            sMinVersionMap.put(Feature.DAEMON, Version.from("8.0.0.r1243"));
+            sMinVersionMap.put(Feature.DAEMON, Version.from("8.0.0.r1264"));
             sMinVersionMap.put(Feature.APP_SHARING, Version.from("8.0.0.r1261"));
             sMinVersionMap.put(Feature.IN_APP_INSTALLATION, Version.from("8.0.0.r1248"));
         } else {
