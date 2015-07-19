f11fc8caa323af9a75781bb16a501a6d4e476ac5
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2015-07-19 11:03:47 -0400

    Android GUI: Bump minimum daemon version for codename changes

--- Android_GUI/src/com/github/chenxiaolong/dualbootpatcher/socket/MbtoolUtils.java
+++ Android_GUI/src/com/github/chenxiaolong/dualbootpatcher/socket/MbtoolUtils.java
@@ -31,7 +31,7 @@ public class MbtoolUtils {
     static {
         if (BuildConfig.BUILD_TYPE.equals("ci")) {
             // Snapshot builds
-            sMinVersionMap.put(Feature.DAEMON, Version.from("8.0.0.r1438"));
+            sMinVersionMap.put(Feature.DAEMON, Version.from("8.0.0.r1456"));
             sMinVersionMap.put(Feature.APP_SHARING, Version.from("8.0.0.r1353"));
             sMinVersionMap.put(Feature.IN_APP_INSTALLATION, Version.from("8.0.0.r1248"));
         } else {
