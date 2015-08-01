8091584b4e6bf84ac51651347b3a6b88db1e2014
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2015-08-01 17:44:53 -0400

    mbtool: Copy media_rw_data_file avtab allow rules to /data/media{/0,}'s SELinux type if they don't match
    Avoids doing a potentially lengthy "restorecon -RF /data/media" on every
    boot, which would break the primary ROM if it's unpatched and doesn't do
    a recursive restorecon itself. This method is very hacky, but allows the
    multibooted ROMs to cope with whatever label the primary ROM put on
    /data/media.

--- mbtool/appsync.cpp
+++ mbtool/appsync.cpp
@@ -151,9 +151,107 @@ static bool load_config_files()
     return true;
 }
 
+static bool copy_avtab_rules(policydb_t *pdb,
+                             const char *from_target_name,
+                             const char *to_target_name)
+{
+    std::vector<std::pair<avtab_key_t, avtab_datum_t>> to_add;
+
+    type_datum_t *from_target, *to_target;
+
+    if (strcmp(from_target_name, to_target_name) == 0) {
+        LOGW("Types %s and %s are equal. Not copying rules",
+             from_target_name, to_target_name);
+        return true;
+    }
+
+    from_target = (type_datum_t *) hashtab_search(
+            pdb->p_types.table, (hashtab_key_t) from_target_name);
+    if (!from_target) {
+        LOGE("(From) Target type %s does not exist", from_target_name);
+        return false;
+    }
+
+    to_target = (type_datum_t *) hashtab_search(
+            pdb->p_types.table, (hashtab_key_t) to_target_name);
+    if (!to_target) {
+        LOGE("(To) Target type %s does not exist", to_target_name);
+        return false;
+    }
+
+    // Gather rules to copy
+    for (uint32_t i = 0; i < pdb->te_avtab.nslot; ++i) {
+        for (avtab_ptr_t cur = pdb->te_avtab.htable[i]; cur; cur = cur->next) {
+            if (!(cur->key.specified & AVTAB_ALLOWED)) {
+                continue;
+            }
+
+            if (cur->key.target_type == from_target->s.value) {
+                avtab_key_t copy = cur->key;
+                copy.target_type = to_target->s.value;
+
+                to_add.push_back(std::make_pair(std::move(copy), cur->datum));
+            }
+        }
+    }
+
+    avtab_datum_t *datum;
+    for (auto &pair : to_add) {
+        datum = avtab_search(&pdb->te_avtab, &pair.first);
+
+        if (!datum) {
+            // Create new avtab rule if the key doesn't exist
+            if (avtab_insert(&pdb->te_avtab, &pair.first, &pair.second) != 0) {
+                // This should absolutely never happen unless libsepol has a bug
+                LOGE("Failed to add rule to avtab");
+                return false;
+            }
+        } else {
+            // Add additional perms if the key already exists
+            datum->data |= pair.second.data;
+        }
+    }
+
+    return true;
+}
+
 /*!
- * \brief Patch SEPolicy to allow installd to connect to our fd
+ * \brief Patch SEPolicy to allow media_data_file-labeled /data/media to work on
+ *        Android >= 5.0
  */
+static bool fix_data_media_rules(policydb_t *pdb)
+{
+    static const char *expected_type = "media_rw_data_file";
+    const char *path = "/data/media/0";
+
+    if (!hashtab_search(pdb->p_types.table, (hashtab_key_t) expected_type)) {
+        LOGW("Type %s doesn't exist. Won't touch /data/media related rules",
+             expected_type);
+        return true;
+    }
+
+    std::string context;
+    if (!util::selinux_lget_context(path, &context)) {
+        LOGE("Failed to get context of %s: %s", path, strerror(errno));
+        path = "/data/media";
+        if (!util::selinux_lget_context(path, &context)) {
+            LOGE("Failed to get context of %s: %s", path, strerror(errno));
+            return false;
+        }
+    }
+
+    std::vector<std::string> pieces = util::split(context, ":");
+    if (pieces.size() < 3) {
+        LOGE("Malformed context string on %s: %s", path, context.c_str());
+        return false;
+    }
+    std::string type = pieces[2];
+
+    LOGV("Copying %s rules to %s because of improper %s SELinux label",
+         expected_type, type.c_str(), path);
+    return copy_avtab_rules(pdb, expected_type, type.c_str());
+}
+
 static bool patch_sepolicy()
 {
     policydb_t pdb;
@@ -174,10 +272,17 @@ static bool patch_sepolicy()
 
     LOGD("Policy version: %u", pdb.policyvers);
 
+    // Make init context permissive
+    util::selinux_make_permissive(&pdb, "init");
+
+    // Allow installd to connect to our socket
     util::selinux_add_rule(&pdb, "installd", "init", "unix_stream_socket", "accept");
     util::selinux_add_rule(&pdb, "installd", "init", "unix_stream_socket", "listen");
     util::selinux_add_rule(&pdb, "installd", "init", "unix_stream_socket", "read");
     util::selinux_add_rule(&pdb, "installd", "init", "unix_stream_socket", "write");
+
+    // Allow access to non-'media_rw_data_file' labeled /data/media
+    fix_data_media_rules(&pdb);
 
     if (!util::selinux_write_policy(SELINUX_LOAD_FILE, &pdb)) {
         LOGE("Failed to write SELinux policy file: %s", SELINUX_LOAD_FILE);
