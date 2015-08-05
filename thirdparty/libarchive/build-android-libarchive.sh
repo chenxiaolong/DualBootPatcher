8aec8674440f9ddb699d75e26d8a98ad15a0382e
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2015-08-04 22:25:56 -0400

    thirdparty: Build libarchive with LZO support

--- thirdparty/libarchive/build-android-libarchive.sh
+++ thirdparty/libarchive/build-android-libarchive.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright (C) 2014  Andrew Gunnerson <andrewgunnerson@gmail.com>
+# Copyright (C) 2014-2015  Andrew Gunnerson <andrewgunnerson@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -17,7 +17,10 @@
 
 set -e
 
+rel=r1
+
 xz_ver='5.2.1'
+lzo_ver='2.09'
 lz4_ver='r128'
 
 url='https://github.com/libarchive/libarchive.git'
@@ -28,6 +31,11 @@ if [ ! -f ../liblzma/liblzma-${xz_ver}_android.tar.bz2 ]; then
     exit 1
 fi
 
+if [ ! -f ../lzo/liblzo2-${lzo_ver}_android.tar.bz2 ]; then
+    echo "Please run thirdparty/lzo/build-android-lzo.sh first"
+    exit 1
+fi
+
 if [ ! -f ../lz4/liblz4-${lz4_ver}_android.tar.bz2 ]; then
     echo "Please run thirdparty/lz4/build-android-lz4.sh first"
     exit 1
@@ -35,6 +43,9 @@ fi
 
 mkdir -p liblzma/
 tar xvf ../liblzma/liblzma-${xz_ver}_android.tar.bz2 -C liblzma/
+
+mkdir -p liblzo2/
+tar xvf ../lzo/liblzo2-${lzo_ver}_android.tar.bz2 -C liblzo2/
 
 mkdir -p liblz4/
 tar xvf ../lz4/liblz4-${lz4_ver}_android.tar.bz2 -C liblz4/
@@ -62,7 +73,7 @@ if [ ! -f android.toolchain.cmake.orig ]; then
     wget 'https://github.com/taka-no-me/android-cmake/raw/master/android.toolchain.cmake' \
         -O android.toolchain.cmake.orig
     # Hack to allow us to specify liblzma and lz4's path
-    sed '/[^A-Z_]CMAKE_FIND_ROOT_PATH[^A-Z_]/ s/)/"${LIBLZMA_PREFIX_PATH}" "${LIBLZ4_PREFIX_PATH}")/g' \
+    sed '/[^A-Z_]CMAKE_FIND_ROOT_PATH[^A-Z_]/ s/)/"${LIBLZMA_PREFIX_PATH}" "${LIBLZO2_PREFIX_PATH}" "${LIBLZ4_PREFIX_PATH}")/g' \
         < android.toolchain.cmake.orig \
         > android.toolchain.cmake
 fi
@@ -80,6 +91,11 @@ build() {
     ln -s "$(pwd)/../../liblzma/include" liblzma/include
     ln -s "$(pwd)/../../liblzma/lib_${abi}" liblzma/lib
 
+    rm -rf liblzo2
+    mkdir liblzo2
+    ln -s "$(pwd)/../../liblzo2/include" liblzo2/include
+    ln -s "$(pwd)/../../liblzo2/lib_${abi}" liblzo2/lib
+
     rm -rf liblz4
     mkdir liblz4
     ln -s "$(pwd)/../../liblz4/include" liblz4/include
@@ -91,6 +107,7 @@ build() {
         -DENABLE_CAT=OFF \
         -DENABLE_TEST=OFF \
         -DLIBLZMA_PREFIX_PATH="$(pwd)/liblzma" \
+        -DLIBLZO2_PREFIX_PATH="$(pwd)/liblzo2" \
         -DLIBLZ4_PREFIX_PATH="$(pwd)/liblz4" \
         -DCMAKE_TOOLCHAIN_FILE=../android.toolchain.cmake \
         -DANDROID_ABI="${abi}" \
@@ -121,7 +138,7 @@ done
 
 curdir="$(pwd)"
 pushd "${outdir}"
-tar jcvf "${curdir}"/../libarchive-${ver}_android.tar.bz2 \
+tar jcvf "${curdir}"/../libarchive-${ver}_${rel}_android.tar.bz2 \
     lib_*/libarchive.a include/
 popd
 
