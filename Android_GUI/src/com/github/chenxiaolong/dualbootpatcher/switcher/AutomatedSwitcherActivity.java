f8fd8541c5a59f94517af7a1065afea56285297b
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2015-08-09 19:19:07 -0400

    Android GUI: Ask for confirmation before switching ROMs via an intent

--- Android_GUI/src/com/github/chenxiaolong/dualbootpatcher/switcher/AutomatedSwitcherActivity.java
+++ Android_GUI/src/com/github/chenxiaolong/dualbootpatcher/switcher/AutomatedSwitcherActivity.java
@@ -20,17 +20,26 @@ package com.github.chenxiaolong.dualbootpatcher.switcher;
 import android.app.Activity;
 import android.app.FragmentManager;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import com.github.chenxiaolong.dualbootpatcher.EventCollector.BaseEvent;
 import com.github.chenxiaolong.dualbootpatcher.EventCollector.EventCollectorListener;
 import com.github.chenxiaolong.dualbootpatcher.R;
+import com.github.chenxiaolong.dualbootpatcher.RomUtils;
 import com.github.chenxiaolong.dualbootpatcher.dialogs.GenericProgressDialog;
+import com.github.chenxiaolong.dualbootpatcher.switcher.ConfirmAutomatedSwitchRomDialog
+        .ConfirmAutomatedSwitchRomDialogListener;
 import com.github.chenxiaolong.dualbootpatcher.switcher.SwitcherEventCollector.SwitchedRomEvent;
 
-public class AutomatedSwitcherActivity extends AppCompatActivity implements EventCollectorListener {
+public class AutomatedSwitcherActivity extends AppCompatActivity implements EventCollectorListener,
+        ConfirmAutomatedSwitchRomDialogListener {
     private static final String TAG = AutomatedSwitcherActivity.class.getSimpleName();
+
+    private static final String PREF_SHOW_CONFIRM_DIALOG = "show_confirm_automated_switcher_dialog";
 
     public static final String EXTRA_ROM_ID = "rom_id";
     public static final String EXTRA_REBOOT = "reboot";
@@ -39,6 +48,8 @@ public class AutomatedSwitcherActivity extends AppCompatActivity implements Even
     public static final String RESULT_MESSAGE = "message";
 
     private SwitcherEventCollector mEventCollector;
+
+    private SharedPreferences mPrefs;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -59,13 +70,16 @@ public class AutomatedSwitcherActivity extends AppCompatActivity implements Even
             fm.beginTransaction().add(mEventCollector, SwitcherEventCollector.TAG).commit();
         }
 
+        mPrefs = getSharedPreferences("settings", 0);
         if (savedInstanceState == null) {
-            GenericProgressDialog d = GenericProgressDialog.newInstance(
-                    R.string.switching_rom, R.string.please_wait);
-            d.show(getFragmentManager(), "automated_switch_rom_waiting");
-
-            mEventCollector.setApplicationContext(getApplicationContext());
-            mEventCollector.chooseRom(getIntent().getStringExtra(EXTRA_ROM_ID));
+            boolean shouldShow = mPrefs.getBoolean(PREF_SHOW_CONFIRM_DIALOG, true);
+            if (shouldShow) {
+                ConfirmAutomatedSwitchRomDialog d = ConfirmAutomatedSwitchRomDialog.newInstance(
+                        getIntent().getStringExtra(EXTRA_ROM_ID));
+                d.show(getFragmentManager(), ConfirmAutomatedSwitchRomDialog.TAG);
+            } else {
+                switchRom();
+            }
         }
     }
 
@@ -104,20 +118,48 @@ public class AutomatedSwitcherActivity extends AppCompatActivity implements Even
             switch (event.result) {
             case SUCCEEDED:
                 intent.putExtra(RESULT_CODE, "SWITCHING_SUCCEEDED");
+                Toast.makeText(this, R.string.choose_rom_success, Toast.LENGTH_SHORT).show();
                 break;
             case FAILED:
                 intent.putExtra(RESULT_CODE, "SWITCHING_FAILED");
                 intent.putExtra(RESULT_MESSAGE,
                         String.format("Failed to switch to %s", event.kernelId));
+                Toast.makeText(this, R.string.choose_rom_failure, Toast.LENGTH_SHORT).show();
                 break;
             case UNKNOWN_BOOT_PARTITION:
                 intent.putExtra(RESULT_CODE, "UNKNOWN_BOOT_PARTITION");
                 intent.putExtra(RESULT_MESSAGE, "Failed to determine boot partition");
+                String codename = RomUtils.getDeviceCodename(this);
+                Toast.makeText(this, getString(R.string.unknown_boot_partition, codename),
+                        Toast.LENGTH_SHORT).show();
                 break;
             }
 
             setResult(Activity.RESULT_OK, intent);
             finish();
         }
+    }
+
+    @Override
+    public void onConfirmSwitchRom(boolean dontShowAgain) {
+        Editor e = mPrefs.edit();
+        e.putBoolean(PREF_SHOW_CONFIRM_DIALOG, !dontShowAgain);
+        e.apply();
+
+        switchRom();
+    }
+
+    @Override
+    public void onCancelSwitchRom() {
+        finish();
+    }
+
+    private void switchRom() {
+        GenericProgressDialog d = GenericProgressDialog.newInstance(
+                R.string.switching_rom, R.string.please_wait);
+        d.show(getFragmentManager(), "automated_switch_rom_waiting");
+
+        mEventCollector.setApplicationContext(getApplicationContext());
+        mEventCollector.chooseRom(getIntent().getStringExtra(EXTRA_ROM_ID));
     }
 }
