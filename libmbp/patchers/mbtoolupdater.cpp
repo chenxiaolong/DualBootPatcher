3799d0bbac836a75cc0246ae9d91928b02c0d0ee
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2015-08-11 01:18:43 -0400

    libmbp: mbtoolupdater.cpp: Don't use regexes for simple whitespace checking

--- libmbp/patchers/mbtoolupdater.cpp
+++ libmbp/patchers/mbtoolupdater.cpp
@@ -19,8 +19,6 @@
 
 #include "patchers/mbtoolupdater.h"
 
-#include <regex>
-
 #include <cassert>
 
 #include "bootimage.h"
@@ -183,6 +181,12 @@ bool MbtoolUpdater::Impl::patchImage()
     return true;
 }
 
+static bool allWhiteSpace(const char *str)
+{
+    for (; *str && isspace(*str); ++str);
+    return *str == '\0';
+}
+
 void MbtoolUpdater::Impl::patchInitRc(CpioFile *cpio)
 {
     std::vector<unsigned char> contents;
@@ -190,14 +194,13 @@ void MbtoolUpdater::Impl::patchInitRc(CpioFile *cpio)
 
     std::vector<std::string> lines = StringUtils::splitData(contents, '\n');
 
-    std::regex whitespace("^\\s*$");
     bool insideService = false;
 
     // Remove old mbtooldaemon service definition
     for (auto it = lines.begin(); it != lines.end();) {
         if (StringUtils::starts_with(*it, "service")) {
             insideService = it->find("mbtooldaemon") != std::string::npos;
-        } else if (insideService && std::regex_search(*it, whitespace)) {
+        } else if (insideService && allWhiteSpace(it->c_str())) {
             insideService = false;
         }
 
