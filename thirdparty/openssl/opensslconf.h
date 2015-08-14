8f1ca178a2347adec93760fb0245c52e676d00b7
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2015-08-14 00:53:25 -0400

    thirdparty: Add scripts for building openssl for Android

---
+++ thirdparty/openssl/opensslconf.h
@@ -0,0 +1,9 @@
+#if defined(__arm__)
+#include "opensslconf_armeabi-v7a.h"
+#elif defined(__aarch64__)
+#include "opensslconf_arm64-v8a.h"
+#elif defined(__i686__)
+#include "opensslconf_x86.h"
+#elif defined(__x86_64__)
+#include "opensslconf_x86_64.h"
+#endif
