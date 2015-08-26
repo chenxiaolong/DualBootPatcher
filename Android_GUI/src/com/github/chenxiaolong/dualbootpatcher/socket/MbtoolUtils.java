2c4042498079fe5d6d6748b97cade27699e59622
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2015-08-26 00:38:37 -0400

    Android GUI: Bump minimum daemon and in-app flashing version for latest changes

--- Android_GUI/src/com/github/chenxiaolong/dualbootpatcher/socket/MbtoolUtils.java
+++ Android_GUI/src/com/github/chenxiaolong/dualbootpatcher/socket/MbtoolUtils.java
@@ -36,9 +36,9 @@ public class MbtoolUtils {
     static {
         if (BuildConfig.BUILD_TYPE.equals("ci")) {
             // Snapshot builds
-            sMinVersionMap.put(Feature.DAEMON, Version.from("8.0.0.r1595"));
+            sMinVersionMap.put(Feature.DAEMON, Version.from("8.0.0.r1649"));
             sMinVersionMap.put(Feature.APP_SHARING, Version.from("8.0.0.r1353"));
-            sMinVersionMap.put(Feature.IN_APP_INSTALLATION, Version.from("8.0.0.r1492"));
+            sMinVersionMap.put(Feature.IN_APP_INSTALLATION, Version.from("8.0.0.r1649"));
         } else {
             // Debug/release builds
             sMinVersionMap.put(Feature.DAEMON, Version.from("8.99.10"));
