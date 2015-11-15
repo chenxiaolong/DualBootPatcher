bd05dcd5365686a90798b2f3a917150ec78a5036
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2015-11-15 16:22:01 -0500

    Android GUI: Regenerate v3 protocol code

--- Android_GUI/src/mbtool/daemon/v3/FileOpenFlag.java
+++ Android_GUI/src/mbtool/daemon/v3/FileOpenFlag.java
@@ -7,11 +7,12 @@ public final class FileOpenFlag {
   public static final short APPEND = 0;
   public static final short CREAT = 1;
   public static final short EXCL = 2;
-  public static final short RDWR = 3;
-  public static final short TRUNC = 4;
-  public static final short WRONLY = 5;
+  public static final short RDONLY = 3;
+  public static final short RDWR = 4;
+  public static final short TRUNC = 5;
+  public static final short WRONLY = 6;
 
-  private static final String[] names = { "APPEND", "CREAT", "EXCL", "RDWR", "TRUNC", "WRONLY", };
+  private static final String[] names = { "APPEND", "CREAT", "EXCL", "RDONLY", "RDWR", "TRUNC", "WRONLY", };
 
   public static String name(int e) { return names[e]; }
 };
