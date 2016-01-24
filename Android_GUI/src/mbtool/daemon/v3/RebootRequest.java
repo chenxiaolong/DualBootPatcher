fd7bfc66e97262498ea493ad68f4eecf7e8aa352
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2016-01-23 19:16:37 -0500

    Android GUI: Regenerate protocol bindings

--- Android_GUI/src/mbtool/daemon/v3/RebootRequest.java
+++ Android_GUI/src/mbtool/daemon/v3/RebootRequest.java
@@ -15,16 +15,24 @@ public final class RebootRequest extends Table {
 
   public String arg() { int o = __offset(4); return o != 0 ? __string(o + bb_pos) : null; }
   public ByteBuffer argAsByteBuffer() { return __vector_as_bytebuffer(4, 1); }
+  public short type() { int o = __offset(6); return o != 0 ? bb.getShort(o + bb_pos) : 0; }
+  public boolean confirm() { int o = __offset(8); return o != 0 ? 0!=bb.get(o + bb_pos) : false; }
 
   public static int createRebootRequest(FlatBufferBuilder builder,
-      int arg) {
-    builder.startObject(1);
+      int arg,
+      short type,
+      boolean confirm) {
+    builder.startObject(3);
     RebootRequest.addArg(builder, arg);
+    RebootRequest.addType(builder, type);
+    RebootRequest.addConfirm(builder, confirm);
     return RebootRequest.endRebootRequest(builder);
   }
 
-  public static void startRebootRequest(FlatBufferBuilder builder) { builder.startObject(1); }
+  public static void startRebootRequest(FlatBufferBuilder builder) { builder.startObject(3); }
   public static void addArg(FlatBufferBuilder builder, int argOffset) { builder.addOffset(0, argOffset, 0); }
+  public static void addType(FlatBufferBuilder builder, short type) { builder.addShort(1, type, 0); }
+  public static void addConfirm(FlatBufferBuilder builder, boolean confirm) { builder.addBoolean(2, confirm, false); }
   public static int endRebootRequest(FlatBufferBuilder builder) {
     int o = builder.endObject();
     return o;
---
+++ Android_GUI/src/mbtool/daemon/v3/RebootType.java
@@ -0,0 +1,15 @@
+// automatically generated, do not modify
+
+package mbtool.daemon.v3;
+
+public final class RebootType {
+  private RebootType() { }
+  public static final short FRAMEWORK = 0;
+  public static final short INIT = 1;
+  public static final short DIRECT = 2;
+
+  private static final String[] names = { "FRAMEWORK", "INIT", "DIRECT", };
+
+  public static String name(int e) { return names[e]; }
+};
+
