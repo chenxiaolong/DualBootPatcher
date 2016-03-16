eba9400ea670526affbdadc0c74744201c233cda
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2016-03-16 17:38:02 -0400

    external: Remove LZO submodule

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -30,7 +30,7 @@ if(NOT ANDROID)
     endif()
 
     # Same logic as CMakeLists.txt from the CMake source
-    set(EXTERNAL_LIBRARIES LIBARCHIVE LIBLZMA LZO LZ4 ZLIB)
+    set(EXTERNAL_LIBRARIES LIBARCHIVE LIBLZMA LZ4 ZLIB)
     foreach(extlib ${EXTERNAL_LIBRARIES})
         if(NOT DEFINED MBP_USE_SYSTEM_LIBRARY_${extlib}
                 AND DEFINED MBP_USE_SYSTEM_LIBRARIES)
@@ -67,13 +67,6 @@ if(NOT ANDROID)
         MBP_USE_SYSTEM_LIBLZMA
         "Use system-installed liblzma"
         "${MBP_USE_SYSTEM_LIBRARY_LIBLZMA}"
-        "NOT MBP_USE_SYSTEM_LIBARCHIVE"
-        ON
-    )
-    cmake_dependent_option(
-        MBP_USE_SYSTEM_LZO
-        "Use system-installed lzo"
-        "${MBP_USE_SYSTEM_LIBRARY_LZO}"
         "NOT MBP_USE_SYSTEM_LIBARCHIVE"
         ON
     )
--- cmake/dependencies/lzo.cmake
+++ cmake/dependencies/lzo.cmake
@@ -4,33 +4,12 @@ if(ANDROID)
     set(MBP_LZO_LIBRARIES
         ${THIRD_PARTY_LZO_DIR}/${ANDROID_ABI}/lib/liblzo2.a)
 else()
-    if(MBP_USE_SYSTEM_LZO)
-        find_path(MBP_LZO_INCLUDES lzo/lzo1x.h)
-        find_library(MBP_LZO_LIBRARIES NAMES lzo2 liblzo2)
+    find_path(MBP_LZO_INCLUDES lzo/lzo1x.h)
+    find_library(MBP_LZO_LIBRARIES NAMES lzo2 liblzo2)
 
-        include(FindPackageHandleStandardArgs)
-        find_package_handle_standard_args(LZO DEFAULT_MSG MBP_LZO_LIBRARIES MBP_LZO_INCLUDES)
-        if(NOT LZO_FOUND)
-            message(FATAL_ERROR "CMAKE_USE_SYSTEM_LZO is ON but LZO is not found!")
-        endif()
-    else()
-        set(ENABLE_STATIC ON CACHE INTERNAL "Build static LZO library.")
-        set(ENABLE_SHARED OFF CACHE INTERNAL "Build shared LZO library.")
-        set(ENABLE_BINARIES OFF CACHE INTERNAL "Build LZO binaries.")
-        set(INSTALL_LIBS OFF CACHE INTERNAL "Install LZO library.")
-        set(INSTALL_DOCS OFF CACHE INTERNAL "Install LZO documentation.")
-        set(INSTALL_HEADERS OFF CACHE INTERNAL "Install LZO headers.")
-
-        add_subdirectory(external/lzo)
-
-        # Linking shared library to lzo's static library, need -fPIC
-        set_target_properties(
-            lzo_static
-            PROPERTIES
-            POSITION_INDEPENDENT_CODE 1
-        )
-
-        set(MBP_LZO_INCLUDES "${CMAKE_SOURCE_DIR}/external/lzo/include")
-        set(MBP_LZO_LIBRARIES lzo_static)
+    include(FindPackageHandleStandardArgs)
+    find_package_handle_standard_args(LZO DEFAULT_MSG MBP_LZO_LIBRARIES MBP_LZO_INCLUDES)
+    if(NOT LZO_FOUND)
+        message(FATAL_ERROR "LZO is not found!")
     endif()
 endif()
