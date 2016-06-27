2c7c8b1fd9f435b6ff32a86645ac8dd98ceb48e0
chautruongthinh <chautruongthinh@tekcafe.vn>
2016-06-28 04:34:42 +0700

    libmbp: add support for VEGA IRON device

--- libmbp/devices/pantech.cpp
+++ libmbp/devices/pantech.cpp
@@ -40,6 +40,20 @@ namespace mbp
         device->setRecoveryBlockDevs({ QCOM_RECOVERY, "/dev/block/mmcblk0p14" });
         device->setExtraBlockDevs({ QCOM_TZ, "/dev/block/mmcblk0p8" });
         devices->push_back(device);
+
+	// PANTECH VEGA IRON
+        device = new Device();
+        device->setId("PANTECH");
+        device->setCodenames({ "ef52s", "ef52l", "ef52k" });
+        device->setName("VEGA IRON 2");
+        device->setBlockDevBaseDirs({ QCOM_BASE_DIR });
+        device->setSystemBlockDevs({ QCOM_SYSTEM, "/dev/block/mmcblk0p14" });
+        device->setCacheBlockDevs({ QCOM_CACHE, "/dev/block/mmcblk0p16" });
+        device->setDataBlockDevs({ QCOM_USERDATA, "/dev/block/mmcblk0p26" });
+        device->setBootBlockDevs({ QCOM_BOOT, "/dev/block/mmcblk0p7" });
+        device->setRecoveryBlockDevs({ QCOM_RECOVERY, "/dev/block/mmcblk0p19" });
+        device->setExtraBlockDevs({ QCOM_TZ, "/dev/block/mmcblk0p8" });
+        devices->push_back(device);
     }
 }
 
