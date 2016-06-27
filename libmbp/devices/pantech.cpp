e23fe322aab8daf98965446ad55eb890efc9a537
chautruongthinh <chautruongthinh@tekcafe.vn>
2016-06-28 04:36:35 +0700

    libmbp: Initial attempt at VEGA IRON 2 Boot UI support

--- libmbp/devices/pantech.cpp
+++ libmbp/devices/pantech.cpp
@@ -29,7 +29,7 @@ namespace mbp
 
         // PANTECH VEGA IRON 2
         device = new Device();
-        device->setId("ef63");
+        device->setId("PANTECH");
         device->setCodenames({ "ef63s", "ef63l", "ef63k" });
         device->setName("VEGA IRON 2");
         device->setBlockDevBaseDirs({ QCOM_BASE_DIR });
@@ -39,7 +39,14 @@ namespace mbp
         device->setBootBlockDevs({ QCOM_BOOT, "/dev/block/mmcblk0p7" });
         device->setRecoveryBlockDevs({ QCOM_RECOVERY, "/dev/block/mmcblk0p14" });
         device->setExtraBlockDevs({ QCOM_TZ, "/dev/block/mmcblk0p8" });
-        devices->push_back(device);
+	device->twOptions()->supported = true;
+    	device->twOptions()->flags = 
+		Device::FLAG_TW_QCOM_RTC_FIX | Device::FLAG_TW_GRAPHICS_FORCE_USE_LINELENGTH;
+    	device->twOptions()->pixelFormat = Device::TwPixelFormat::RGBX_8888;
+    	device->twOptions()->brightnessPath = "/sys/class/leds/lcd-backlight/brightness";
+    	device->twOptions()->maxBrightness = 255;
+    	device->twOptions()->defaultBrightness = 160;
+	devices->push_back(device);
 
 	// PANTECH VEGA IRON
         device = new Device();
