9afcd59349df5c74c302ff88400481313e52e0b5
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2016-06-26 23:26:02 -0400

    libmbp: Fix typo from PR

--- libmbp/devices/motorola.cpp
+++ libmbp/devices/motorola.cpp
@@ -94,10 +94,10 @@ void addMotorolaDevices(std::vector<Device *> *devices)
     device->setBlockDevBaseDirs({ QCOM_BASE_DIR });
     device->setSystemBlockDevs({ QCOM_SYSTEM, "/dev/block/mmcblk0p39" });
     device->setCacheBlockDevs({ QCOM_CACHE, "/dev/block/mmcblk0p40" });
-    device->setDataBlockDevs({ QCOM_USERDATA, "/dev/block/mmcblk0p41" });    
+    device->setDataBlockDevs({ QCOM_USERDATA, "/dev/block/mmcblk0p41" });
     device->setBootBlockDevs({ QCOM_BOOT, "/dev/block/mmcblk0p32" });
     device->setRecoveryBlockDevs({ QCOM_RECOVERY, "/dev/block/mmcblk0p33" });
-    devices.push_back(device);
+    devices->push_back(device);
 
     // Motorola Moto E (2nd gen 4G)
     device = new Device();
