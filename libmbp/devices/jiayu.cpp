3802cae15e66db7bb88d72353ebccd132990329a
hankching <hankching@qq.com>
2016-06-28 12:15:15 +0800

    libmbp: Initial attempt at Jiayu S3 Boot UI support

--- libmbp/devices/jiayu.cpp
+++ libmbp/devices/jiayu.cpp
@@ -28,12 +28,12 @@ void addJiayuDevices(std::vector<Device *> *devices)
 {
     Device *device;
 
-    // Jiayu S3
+    // Jiayu S3 Boot UI Test
     device = new Device();
     device->setId("h560");
     device->setArchitecture(ARCH_ARM64_V8A);
-    device->setCodenames({ "h560", "s3_h560", "Jiayu_S3", "Jiayu-S3","JY_S3", "JY-S3"});
-    device->setName("Jiayu S3");
+    device->setCodenames({ "h560", "s3_h560", "Jiayu_S3", "Jiayu-S3", "JY_S3", "JY-S3"});
+    device->setName("Jiayu S3 Boot UI Test");
     device->setBlockDevBaseDirs({ MTK_BASE_DIR, MTK_11230000_BASE_DIR });
     device->setSystemBlockDevs({ MTK_SYSTEM, MTK_11230000_SYSTEM,
                                  "/dev/block/mmcblk0p17" });
@@ -54,6 +54,18 @@ void addJiayuDevices(std::vector<Device *> *devices)
         MTK_TEE2, MTK_11230000_TEE2,
         MTK_UBOOT, MTK_11230000_UBOOT
     });
+    device->twOptions()->supported = true;
+    device->twOptions()->flags =
+              Device::FLAG_TW_IGNORE_MAJOR_AXIS_0
+            | Device::FLAG_TW_PREFER_LCD_BACKLIGHT
+            | Device::FLAG_TW_QCOM_RTC_FIX
+            | Device::FLAG_TW_GRAPHICS_FORCE_USE_LINELENGTH
+            | Device::FLAG_TW_HAS_DOWNLOAD_MODE;
+    device->twOptions()->graphicsBackends = { "fbdev" };
+    device->twOptions()->pixelFormat = Device::TwPixelFormat::RGBX_8888;
+    device->twOptions()->brightnessPath = "/sys/class/leds/lcd-backlight/brightness";
+    device->twOptions()->maxBrightness = 255;
+    device->twOptions()->defaultBrightness = 160;
     devices->push_back(device);
 }
 
