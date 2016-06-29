bf8c8f24dce74e9b17720bf57f41ad907ddfcbbd
Caio Oliveira <caiooliveirafarias0@gmail.com>
2016-06-29 17:37:27 -0300

    libmbp: devices: Update Copyright to 2016
    * It's not my job, but it's cool :)
    
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>

--- libmbp/devices/sony.cpp
+++ libmbp/devices/sony.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015  Andrew Gunnerson <andrewgunnerson@gmail.com>
+ * Copyright (C) 2015-2016  Andrew Gunnerson <andrewgunnerson@gmail.com>
  *
  * This file is part of MultiBootPatcher
  *
