e7823658ed6a4196e8fdcd4555a57f6e8c1bbbdd
hankching <hankching@qq.com>
2016-07-01 08:15:26 +0800

    Update fbdev

--- libmbp/devices/jiayu.cpp
+++ libmbp/devices/jiayu.cpp
@@ -54,13 +54,13 @@ void addJiayuDevices(std::vector<Device *> *devices)
         MTK_TEE2, MTK_11230000_TEE2,
         MTK_UBOOT, MTK_11230000_UBOOT });
     device->twOptions()->supported = true;
+    device->twOptions()->graphicsBackends = { "fbdev" };
     device->twOptions()->flags = Device::FLAG_TW_GRAPHICS_FORCE_USE_LINELENGTH
                                | Device::FLAG_TW_HAS_DOWNLOAD_MODE;
     device->twOptions()->pixelFormat = Device::TwPixelFormat::RGBX_8888;
     device->twOptions()->maxBrightness = 255;
     device->twOptions()->defaultBrightness = 162;
     device->twOptions()->cpuTempPath = "/sys/class/thermal/thermal_zone1/temp";
-#    device->twOptions()->graphicsBackends = { "fbdev" };
     devices->push_back(device);
 }
 
