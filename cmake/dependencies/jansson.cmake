faff40993bbedc5674d597493b994f8c86a1b9d6
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2016-08-13 19:07:17 -0400

    cmake: Add support for finding jansson on the host

--- cmake/dependencies/jansson.cmake
+++ cmake/dependencies/jansson.cmake
@@ -3,4 +3,15 @@ if(ANDROID)
         ${THIRD_PARTY_JANSSON_DIR}/${ANDROID_ABI}/include)
     set(MBP_JANSSON_LIBRARIES
         ${THIRD_PARTY_JANSSON_DIR}/${ANDROID_ABI}/lib/libjansson.a)
+else()
+    find_path(MBP_JANSSON_INCLUDES jansson.h)
+    find_library(MBP_JANSSON_LIBRARIES NAMES jansson libjansson)
+
+    include(FindPackageHandleStandardArgs)
+    find_package_handle_standard_args(
+        JANSSON DEFAULT_MSG MBP_JANSSON_LIBRARIES MBP_JANSSON_INCLUDES
+    )
+    if(NOT JANSSON_FOUND)
+        message(FATAL_ERROR "jansson library was not found")
+    endif()
 endif()
