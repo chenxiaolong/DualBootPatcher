f00f5cd792cfc7adac6150760a83b93f8ffbc01b
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2016-09-04 15:05:57 -0400

    mbtool: Move /data/media SELinux policy patching to sepolpatch.cpp

--- mbtool/sepolpatch.cpp
+++ mbtool/sepolpatch.cpp
@@ -20,7 +20,10 @@
 #include "sepolpatch.h"
 
 #include <memory>
+#include <vector>
+
 #include <cstdio>
+
 #include <fcntl.h>
 #include <getopt.h>
 #include <sys/mman.h>
@@ -33,6 +36,9 @@
 #include "mblog/logging.h"
 #include "mbutil/autoclose/file.h"
 #include "mbutil/selinux.h"
+#include "mbutil/string.h"
+
+#include "multiboot.h"
 
 
 namespace mb
@@ -46,6 +52,107 @@ static const char *permissive_types[] = {
     //"recovery",
     nullptr
 };
+
+static bool copy_avtab_rules(policydb_t *pdb,
+                             const char *from_target_name,
+                             const char *to_target_name)
+{
+    std::vector<std::pair<avtab_key_t, avtab_datum_t>> to_add;
+
+    type_datum_t *from_target, *to_target;
+
+    if (strcmp(from_target_name, to_target_name) == 0) {
+        LOGW("Types %s and %s are equal. Not copying rules",
+             from_target_name, to_target_name);
+        return true;
+    }
+
+    from_target = (type_datum_t *) hashtab_search(
+            pdb->p_types.table, (hashtab_key_t) from_target_name);
+    if (!from_target) {
+        LOGE("(From) Target type %s does not exist", from_target_name);
+        return false;
+    }
+
+    to_target = (type_datum_t *) hashtab_search(
+            pdb->p_types.table, (hashtab_key_t) to_target_name);
+    if (!to_target) {
+        LOGE("(To) Target type %s does not exist", to_target_name);
+        return false;
+    }
+
+    // Gather rules to copy
+    for (uint32_t i = 0; i < pdb->te_avtab.nslot; ++i) {
+        for (avtab_ptr_t cur = pdb->te_avtab.htable[i]; cur; cur = cur->next) {
+            if (!(cur->key.specified & AVTAB_ALLOWED)) {
+                continue;
+            }
+
+            if (cur->key.target_type == from_target->s.value) {
+                avtab_key_t copy = cur->key;
+                copy.target_type = to_target->s.value;
+
+                to_add.push_back(std::make_pair(std::move(copy), cur->datum));
+            }
+        }
+    }
+
+    avtab_datum_t *datum;
+    for (auto &pair : to_add) {
+        datum = avtab_search(&pdb->te_avtab, &pair.first);
+
+        if (!datum) {
+            // Create new avtab rule if the key doesn't exist
+            if (avtab_insert(&pdb->te_avtab, &pair.first, &pair.second) != 0) {
+                // This should absolutely never happen unless libsepol has a bug
+                LOGE("Failed to add rule to avtab");
+                return false;
+            }
+        } else {
+            // Add additional perms if the key already exists
+            datum->data |= pair.second.data;
+        }
+    }
+
+    return true;
+}
+
+/*!
+ * \brief Patch SEPolicy to allow media_data_file-labeled /data/media to work on
+ *        Android >= 5.0
+ */
+static bool fix_data_media_rules(policydb_t *pdb)
+{
+    static const char *expected_type = "media_rw_data_file";
+    const char *path = INTERNAL_STORAGE;
+
+    if (!hashtab_search(pdb->p_types.table, (hashtab_key_t) expected_type)) {
+        LOGW("Type %s doesn't exist. Won't touch /data/media related rules",
+             expected_type);
+        return true;
+    }
+
+    std::string context;
+    if (!util::selinux_lget_context(path, &context)) {
+        LOGE("Failed to get context of %s: %s", path, strerror(errno));
+        path = "/data/media";
+        if (!util::selinux_lget_context(path, &context)) {
+            LOGE("Failed to get context of %s: %s", path, strerror(errno));
+            return false;
+        }
+    }
+
+    std::vector<std::string> pieces = util::split(context, ":");
+    if (pieces.size() < 3) {
+        LOGE("Malformed context string on %s: %s", path, context.c_str());
+        return false;
+    }
+    std::string type = pieces[2];
+
+    LOGV("Copying %s rules to %s because of improper %s SELinux label",
+         expected_type, type.c_str(), path);
+    return copy_avtab_rules(pdb, expected_type, type.c_str());
+}
 
 static bool patch_sepolicy_internal(const std::string &source,
                                     const std::string &target)
@@ -68,6 +175,8 @@ static bool patch_sepolicy_internal(const std::string &source,
     for (const char **iter = permissive_types; *iter; ++iter) {
         util::selinux_make_permissive(&pdb, *iter);
     }
+
+    fix_data_media_rules(&pdb);
 
     if (!util::selinux_write_policy(target, &pdb)) {
         LOGE("Failed to write SELinux policy file: %s", target.c_str());
