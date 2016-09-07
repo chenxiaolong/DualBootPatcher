3bbb5c7d3ad75021008bd20100a50c98099d9afa
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2016-09-06 21:53:22 -0400

    yaml2json: Drop jsoncpp dependency and use jansson

--- yaml2json/yaml2json.cpp
+++ yaml2json/yaml2json.cpp
@@ -3,48 +3,59 @@
 
 #include <getopt.h>
 
-#include <json/json.h>
+#include <jansson.h>
 #include <yaml-cpp/yaml.h>
 
-#define TRY_RETURN(NODE, AS) \
-    try { \
-        return Json::Value((NODE).as<AS>()); \
-    } catch (const YAML::BadConversion &e) { \
-    }
 
-static Json::Value yaml_node_to_json_node(const YAML::Node &yaml_node)
+static void * xmalloc(size_t size)
+{
+    void *result = malloc(size);
+    if (!result) {
+        abort();
+    }
+    return result;
+}
+
+static json_t * yaml_node_to_json_node(const YAML::Node &yaml_node)
 {
     switch (yaml_node.Type()) {
     case YAML::NodeType::Null:
-        return Json::Value();
+        return json_object();
 
     case YAML::NodeType::Scalar:
-        TRY_RETURN(yaml_node, Json::Value::UInt64);
-        TRY_RETURN(yaml_node, Json::Value::Int64);
-        TRY_RETURN(yaml_node, double);
-        TRY_RETURN(yaml_node, bool);
-        TRY_RETURN(yaml_node, std::string);
+        try {
+            return json_integer(yaml_node.as<json_int_t>());
+        } catch (const YAML::BadConversion &e) {}
+        try {
+            return json_real(yaml_node.as<double>());
+        } catch (const YAML::BadConversion &e) {}
+        try {
+            return json_boolean(yaml_node.as<bool>());
+        } catch (const YAML::BadConversion &e) {}
+        try {
+            return json_string(yaml_node.as<std::string>().c_str());
+        } catch (const YAML::BadConversion &e) {}
         throw std::runtime_error("Cannot convert scalar value to known type");
 
     case YAML::NodeType::Sequence: {
-        Json::Value value(Json::arrayValue);
+        json_t *array = json_array();
 
         for (auto const &item : yaml_node) {
-            value.append(yaml_node_to_json_node(item));
+            json_array_append_new(array, yaml_node_to_json_node(item));
         }
 
-        return value;
+        return array;
     }
 
     case YAML::NodeType::Map: {
-        Json::Value value(Json::objectValue);
+        json_t *object = json_object();
 
         for (auto const &item : yaml_node) {
-            value[item.first.as<std::string>()] =
-                    yaml_node_to_json_node(item.second);
+            json_object_set_new(object, item.first.as<std::string>().c_str(),
+                                yaml_node_to_json_node(item.second));
         }
 
-        return value;
+        return object;
     }
 
     case YAML::NodeType::Undefined:
@@ -117,32 +128,32 @@ int main(int argc, char *argv[])
         }
     }
 
-    Json::Value json_root(Json::arrayValue);
+    json_set_alloc_funcs(&xmalloc, &free);
+
+    json_t *json_root = json_array();
 
     for (int i = optind; i < argc; ++i) {
         try {
             YAML::Node root = YAML::LoadFile(argv[i]);
-            Json::Value node = yaml_node_to_json_node(root);
+            json_t *node = yaml_node_to_json_node(root);
 
-            if (flatten_root_array && node.type() == Json::arrayValue) {
-                for (auto const &elem : node) {
-                    json_root.append(elem);
+            if (flatten_root_array && json_is_array(node)) {
+                size_t index;
+                json_t *elem;
+
+                json_array_foreach(node, index, elem) {
+                    json_array_append(json_root, elem);
                 }
+
+                json_decref(node);
             } else {
-                json_root.append(node);
+                json_array_append_new(json_root, node);
             }
         } catch (const std::exception &e) {
             fprintf(stderr, "%s: Failed to convert file: %s\n",
                     argv[i], e.what());
             return EXIT_FAILURE;
         }
-    }
-
-    std::string output;
-    if (styled) {
-        output = Json::StyledWriter().write(json_root);
-    } else {
-        output = Json::FastWriter().write(json_root);
     }
 
     FILE *fp = stdout;
@@ -156,9 +167,19 @@ int main(int argc, char *argv[])
         }
     }
 
-    if (fputs(output.c_str(), fp) == EOF) {
+    char *output;
+    if (styled) {
+        output = json_dumps(json_root, JSON_INDENT(4) | JSON_SORT_KEYS);
+    } else {
+        output = json_dumps(json_root, JSON_COMPACT);
+    }
+
+    if (fputs(output, fp) == EOF) {
         fprintf(stderr, "Failed to write JSON: %s\n", strerror(errno));
     }
+
+    free(output);
+    json_decref(json_root);
 
     if (output_file) {
         if (fclose(fp) != 0) {
