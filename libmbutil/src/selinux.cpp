12e8949c23e6b42364547a25542bd2353f4a3018
Andrew Gunnerson <chenxiaolong@cxl.epac.to>
2017-09-09 19:19:56 -0400

    libmbcommon,libmbutil,mbtool: Update to use ErrorRestorer
    Signed-off-by: Andrew Gunnerson <chenxiaolong@cxl.epac.to>

--- libmbutil/src/selinux.cpp
+++ libmbutil/src/selinux.cpp
@@ -30,6 +30,7 @@
 
 #include <sepol/sepol.h>
 
+#include "mbcommon/error.h"
 #include "mbcommon/finally.h"
 #include "mblog/logging.h"
 #include "mbutil/fts.h"
@@ -419,9 +420,8 @@ bool selinux_get_process_attr(pid_t pid, SELinuxAttr attr,
     }
 
     auto close_fd = finally([&]{
-        int saved_errno = errno;
+        ErrorRestorer restorer;
         close(fd);
-        errno = saved_errno;
     });
 
     std::vector<char> buf(sysconf(_SC_PAGE_SIZE));
@@ -449,9 +449,8 @@ bool selinux_set_process_attr(pid_t pid, SELinuxAttr attr,
     }
 
     auto close_fd = finally([&]{
-        int saved_errno = errno;
+        ErrorRestorer restorer;
         close(fd);
-        errno = saved_errno;
     });
 
     ssize_t n;
