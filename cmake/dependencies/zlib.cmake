a3236f4c03e89a40c3c6f2f5dfea76a34d98c874
Andrew Gunnerson <chenxiaolong@cxl.epac.to>
2017-12-29 23:04:27 -0500

    cmake: Remove zlib unified headers warning since the old sysroots are gone
    Signed-off-by: Andrew Gunnerson <chenxiaolong@cxl.epac.to>

--- cmake/dependencies/zlib.cmake
+++ cmake/dependencies/zlib.cmake
@@ -1,6 +1,1 @@
 find_package(ZLIB REQUIRED)
-
-if(ANDROID AND ANDROID_UNIFIED_HEADERS
-        AND NOT ${ZLIB_INCLUDE_DIR} MATCHES "/sysroot/usr/include$")
-    message(FATAL_ERROR "zlib.h was found in legacy directory instead of unified. Please clean your build directory.")
-endif()
