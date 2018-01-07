306f3c6341d17df243e7eb1ced96ecd0ce1c4472
Andrew Gunnerson <chenxiaolong@cxl.epac.to>
2018-01-06 20:49:55 -0500

    cmake: Only build one variant of minizip
    None of the current build targets need both the shared and static
    libraries at the same time.
    
    Signed-off-by: Andrew Gunnerson <chenxiaolong@cxl.epac.to>

--- cmake/dependencies/minizip.cmake
+++ cmake/dependencies/minizip.cmake
@@ -3,38 +3,45 @@ set(USE_BZIP2 OFF CACHE BOOL "Enables building with BZIP2 library" FORCE)
 set(USE_LZMA OFF CACHE BOOL "Enables building with LZMA library" FORCE)
 set(USE_CRYPT OFF CACHE BOOL "Enables building with PKWARE traditional encryption" FORCE)
 set(USE_AES OFF CACHE BOOL "Enables building with AES library" FORCE)
+
+backup_variable(BUILD_SHARED_LIBS)
+
+if(NOT ${MBP_BUILD_TARGET} STREQUAL android-system)
+    set(BUILD_SHARED_LIBS TRUE)
+endif()
+
 add_subdirectory(external/minizip)
 
-foreach(_target libminizip-shared libminizip-static)
-    if(NOT MSVC)
-        target_compile_options(
-            ${_target}
-            PRIVATE
-            -Wno-cast-qual
-            -Wno-conversion
-            -Wno-empty-body
-            -Wno-missing-declarations
-            -Wno-missing-field-initializers
-            -Wno-missing-prototypes
-            -Wno-pedantic
-            -Wno-sign-compare
-            -Wno-sign-conversion
-            -Wno-unused-parameter
-            -Wno-unused-variable
-        )
+restore_variable(BUILD_SHARED_LIBS)
 
-        if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
-            target_compile_options(
-                ${_target}
-                PRIVATE
-                -Wno-missing-variable-declarations
-            )
-        else()
-            target_compile_options(
-                ${_target}
-                PRIVATE
-                -Wno-unused-but-set-variable
-            )
-        endif()
+if(NOT MSVC)
+    target_compile_options(
+        libminizip
+        PRIVATE
+        -Wno-cast-qual
+        -Wno-conversion
+        -Wno-empty-body
+        -Wno-missing-declarations
+        -Wno-missing-field-initializers
+        -Wno-missing-prototypes
+        -Wno-pedantic
+        -Wno-sign-compare
+        -Wno-sign-conversion
+        -Wno-unused-parameter
+        -Wno-unused-variable
+    )
+
+    if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+        target_compile_options(
+            libminizip
+            PRIVATE
+            -Wno-missing-variable-declarations
+        )
+    else()
+        target_compile_options(
+            libminizip
+            PRIVATE
+            -Wno-unused-but-set-variable
+        )
     endif()
-endforeach()
+endif()
