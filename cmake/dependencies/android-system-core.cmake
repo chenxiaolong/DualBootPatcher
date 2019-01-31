a8fa542905802af47d5fa2c5c59e7c0c5b8ac695
Andrew Gunnerson <andrewgunnerson@gmail.com>
2019-01-30 23:30:54 -0500

    cmake: Use bundled android-system-core instead of prebuilts
    Signed-off-by: Andrew Gunnerson <andrewgunnerson@gmail.com>

--- cmake/dependencies/android-system-core.cmake
+++ cmake/dependencies/android-system-core.cmake
@@ -1,4 +1,1 @@
-find_package(AndroidLibCutils REQUIRED)
-find_package(AndroidLibLog REQUIRED)
-find_package(AndroidLibUtils REQUIRED)
-find_package(AndroidPixelFlinger REQUIRED)
+add_subdirectory(external/android-system-core)
