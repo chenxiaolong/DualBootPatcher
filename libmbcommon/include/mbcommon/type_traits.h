fa471e8fb935c5a1e01ead6933a5e1a287155a47
Andrew Gunnerson <andrewgunnerson@gmail.com>
2019-04-05 22:32:20 -0400

    libmbcommon: type_traits.h: Remove MSVC-incompatible types
    Signed-off-by: Andrew Gunnerson <andrewgunnerson@gmail.com>

--- libmbcommon/include/mbcommon/type_traits.h
+++ libmbcommon/include/mbcommon/type_traits.h
@@ -21,76 +21,10 @@
 
 #include <type_traits>
 
-#include <cstddef>
-
 namespace mb
 {
 
 template<auto fn>
-using TypeFn = std::integral_constant<decltype(fn), fn>;
-
-// Based on https://stackoverflow.com/a/43526780
-
-// Get nth template argument type
-
-template <std::size_t N, typename T0, typename... Ts>
-struct TypeN
-{
-    using type = typename TypeN<N - 1, Ts...>::type;
-};
-
-template <typename T0, typename... Ts>
-struct TypeN<0, T0, Ts...>
-{
-    using type = T0;
-};
-
-// Get nth function argument type
-
-template <std::size_t, typename>
-struct ArgN;
-
-template <std::size_t N, typename R, typename... As>
-struct ArgN<N, R(As...)>
-{
-    using type = typename TypeN<N, As...>::type;
-};
-
-template <std::size_t N, typename R, typename... As>
-struct ArgN<N, R(As...) noexcept>
-{
-    using type = typename TypeN<N, As...>::type;
-};
-
-// Get function return type
-
-template <typename>
-struct ReturnType;
-
-template <typename R, typename... As>
-struct ReturnType<R(As...)>
-{
-    using type = R;
-};
-
-template <typename R, typename... As>
-struct ReturnType<R(As...) noexcept>
-{
-    using type = R;
-};
-
-#ifdef _WIN32
-template <typename R, typename... As>
-struct ReturnType<R __stdcall(As...)>
-{
-    using type = R;
-};
-
-template <typename R, typename... As>
-struct ReturnType<R __stdcall(As...) noexcept>
-{
-    using type = R;
-};
-#endif
+using TypeFn = std::integral_constant<std::decay_t<decltype(fn)>, fn>;
 
 }
